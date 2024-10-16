20d9c1215c8d54b7999ee0288195429a321d9758
Lee Youl <2yulrang@gmail.com>
2024-10-16 17:48:50 +0900

    🔧 update configure

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,84 +1,76 @@
 module.exports = {
-    root: true,
-    parser: '@typescript-eslint/parser',
-    parserOptions: {
-      ecmaVersion: 2020,
-      sourceType: 'module',
-      ecmaFeatures: {
-        jsx: true,
-      },
-      project: './tsconfig.json',
-      createDefaultProgram: true,
+  root: true,
+  parser: '@typescript-eslint/parser',
+  parserOptions: {
+    ecmaVersion: 2020,
+    sourceType: 'module',
+    ecmaFeatures: {
+      jsx: true,
     },
-    env: {
-      browser: true,
-      node: true,
-      es6: true,
-    },
-    extends: [
-      'eslint:recommended',
-      'plugin:react/recommended',
-      'plugin:react-hooks/recommended',
-      'plugin:@typescript-eslint/recommended',
-      'airbnb',
-      'airbnb/hooks',
-      'airbnb-typescript',
-      'plugin:jsx-a11y/recommended',
-      'next/core-web-vitals',
-      'plugin:prettier/recommended',
-    ],
-    plugins: [
-      'react',
-      'react-hooks',
-      '@typescript-eslint',
-      'jsx-a11y',
-      'import',
-      'prettier',
-    ],
-    rules: {
-      'prettier/prettier': [
-        'error',
-        {
-          endOfLine: 'auto',
-        },
-      ],
-      'jsx-a11y/label-has-associated-control': [
-        'error',
-        {
-          required: {
-            some: ['nesting', 'id'],
-          },
-        },
-      ],
-      "react/react-in-jsx-scope": "off", // Next.js doesn't require React to be in scope
-      "react/prop-types": "off",
-      "no-console": "error",
-      "react/jsx-props-no-spreading": "off",
-      '@typescript-eslint/semi': 'off',
-      'react/jsx-filename-extension': ['warn', { extensions: ['.ts', '.tsx'] }],
-      'no-useless-catch': 'off',
-      '@typescript-eslint/no-unused-vars': 'warn',
-      'react/require-default-props': 'off',
-      'react/jsx-no-useless-fragment': 'warn',
-      'react-hooks/exhaustive-deps': 'off',
-      'import/prefer-default-export': 'off',
-    },
-    settings: {
-      react: {
-        version: 'detect',
-      },
-      'import/resolver': {
-        typescript: {},
-      },
-    },
-    overrides: [
+    project: './tsconfig.json',
+    createDefaultProgram: true,
+  },
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  extends: [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:react-hooks/recommended',
+    'plugin:@typescript-eslint/recommended',
+    'airbnb',
+    'airbnb/hooks',
+    'airbnb-typescript',
+    'plugin:jsx-a11y/recommended',
+    'next/core-web-vitals',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['react', 'react-hooks', '@typescript-eslint', 'jsx-a11y', 'import', 'prettier'],
+  rules: {
+    'prettier/prettier': [
+      'error',
       {
-        files: ['*.tsx', '*.jsx'],
-        rules: {
-          '@typescript-eslint/no-use-before-define': 'off',
+        endOfLine: 'lf',
+      },
+    ],
+    'jsx-a11y/label-has-associated-control': [
+      'error',
+      {
+        required: {
+          some: ['nesting', 'id'],
         },
       },
     ],
-    ignorePatterns: ['next.config.mjs', 'postcss.config.mjs'],
-  };
-  
+    'react/react-in-jsx-scope': 'off', // Next.js doesn't require React to be in scope
+    'react/prop-types': 'off',
+    'no-console': 'error',
+    'react/jsx-props-no-spreading': 'off',
+    '@typescript-eslint/semi': 'off',
+    'react/jsx-filename-extension': ['warn', { extensions: ['.ts', '.tsx'] }],
+    'no-useless-catch': 'off',
+    '@typescript-eslint/no-unused-vars': 'warn',
+    'react/require-default-props': 'off',
+    'react/jsx-no-useless-fragment': 'warn',
+    'react-hooks/exhaustive-deps': 'off',
+    'import/prefer-default-export': 'off',
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+    'import/resolver': {
+      typescript: {},
+    },
+  },
+  overrides: [
+    {
+      files: ['*.tsx', '*.jsx'],
+      rules: {
+        '@typescript-eslint/no-use-before-define': 'off',
+      },
+    },
+  ],
+  ignorePatterns: ['next.config.mjs', 'postcss.config.mjs'],
+};
--- .prettierrc.js
+++ .prettierrc.js
@@ -1,34 +1,30 @@
 module.exports = {
-    arrowParens: 'always',
-    endOfLine: 'lf',
-    printWidth: 80,
-    quoteProps: 'as-needed',
-    semi: true,
-    singleQuote: true,
-    tabWidth: 2,
-    useTabs: false,
-  
-    importOrder: [
-      '^react',
-      '<THIRD_PARTY_MODULES>',
-      '^@/src/apis/(.*)$',
-      '^@/src/hooks/(.*)$',
-      '^@/src/app/(.*)$',
-      '^@/src/components/(.*)$',
-      '^@/src/styles/(.*)$',
-      '^@core/(.*)$',
-      '^@server/(.*)$',
-      '^@ui/(.*)$',
-      '^[./]',
-    ],
-    importOrderSeparation: true,
-    importOrderSortSpecifiers: true,
-  
-    plugins: [
-      '@trivago/prettier-plugin-sort-imports',
-      'prettier-plugin-tailwindcss',
-    ],
-  
-    tailwindConfig: './tailwind.config.ts',
-  };
-  
+  arrowParens: 'always',
+  endOfLine: 'auto',
+  printWidth: 100,
+  quoteProps: 'as-needed',
+  semi: true,
+  singleQuote: true,
+  tabWidth: 2,
+  useTabs: false,
+
+  importOrder: [
+    '^react',
+    '<THIRD_PARTY_MODULES>',
+    '^@/src/apis/(.*)$',
+    '^@/src/hooks/(.*)$',
+    '^@/src/app/(.*)$',
+    '^@/src/components/(.*)$',
+    '^@/src/styles/(.*)$',
+    '^@core/(.*)$',
+    '^@server/(.*)$',
+    '^@ui/(.*)$',
+    '^[./]',
+  ],
+  importOrderSeparation: true,
+  importOrderSortSpecifiers: true,
+
+  plugins: ['@trivago/prettier-plugin-sort-imports', 'prettier-plugin-tailwindcss'],
+
+  tailwindConfig: './tailwind.config.ts',
+};
