d31fb7f8f71ba4f2ffff72802550ac020ec6542f
min5x5 <minkyung5x5@gmail.com>
2024-10-16 16:37:34 +0900

    👷 chore: eslint, prettier 설치

---
+++ .prettierrc.js
@@ -0,0 +1,34 @@
+module.exports = {
+    arrowParens: 'always',
+    endOfLine: 'lf',
+    printWidth: 80,
+    quoteProps: 'as-needed',
+    semi: true,
+    singleQuote: true,
+    tabWidth: 2,
+    useTabs: false,
+  
+    importOrder: [
+      '^react',
+      '<THIRD_PARTY_MODULES>',
+      '^@/src/apis/(.*)$',
+      '^@/src/hooks/(.*)$',
+      '^@/src/app/(.*)$',
+      '^@/src/components/(.*)$',
+      '^@/src/styles/(.*)$',
+      '^@core/(.*)$',
+      '^@server/(.*)$',
+      '^@ui/(.*)$',
+      '^[./]',
+    ],
+    importOrderSeparation: true,
+    importOrderSortSpecifiers: true,
+  
+    plugins: [
+      '@trivago/prettier-plugin-sort-imports',
+      'prettier-plugin-tailwindcss',
+    ],
+  
+    tailwindConfig: './tailwind.config.ts',
+  };
+  
