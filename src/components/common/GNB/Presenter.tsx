73b703b71ab19a2e4355f8b1d408f1bdd4e71f57
HaeJungg <gpwnd0120@gmail.com>
2024-10-17 15:26:28 +0900

    ✨feat: gnb presenter

---
+++ src/components/common/GNB/Presenter.tsx
@@ -0,0 +1,57 @@
+'use client';
+
+import Link from 'next/link';
+
+interface GNBPresenterProps {
+  hasCookie: boolean;
+  // profileImageUrl: string;
+  handleLogout: () => void;
+  handleDropdown: () => void;
+  isDropdownOpen: boolean;
+}
+
+export default function GNBPresenter({
+  hasCookie,
+  // profileImageUrl,
+  handleLogout,
+  handleDropdown,
+  isDropdownOpen,
+}: GNBPresenterProps) {
+  return (
+    <nav className="flex items-center justify-between px-4 py-2">
+      <Link href="/" className="flex items-center">
+        크루크루
+      </Link>
+      <ul className="flex items-center">
+        <li>
+          <Link href="/">모임 찾기</Link>
+        </li>
+        <li>
+          {hasCookie ? <Link href="/likes">찜한 모임</Link> : <Link href="/login">찜한 모임</Link>}
+        </li>
+        {hasCookie ? (
+          <li className="relative">
+            <button type="button" onClick={handleDropdown}>
+              {/* profileImageUrl */}
+              프로필이미지
+            </button>
+            {isDropdownOpen && (
+              <div className="absolute bg-white">
+                <Link href="/profile" className="block" onClick={handleDropdown}>
+                  마이페이지
+                </Link>
+                <button type="button" onClick={handleLogout} className="block">
+                  로그아웃
+                </button>
+              </div>
+            )}
+          </li>
+        ) : (
+          <li>
+            <Link href="/login">로그인</Link>
+          </li>
+        )}
+      </ul>
+    </nav>
+  );
+}
