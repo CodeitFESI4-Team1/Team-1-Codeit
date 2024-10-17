cd1cbfa6c7077ec62a3d95e97e6564c78bdb0edc
Lee Youl <2yulrang@gmail.com>
2024-10-17 13:38:32 +0900

    🔧 chore: tailwind config 수정

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -6,16 +6,32 @@ const config: Config = {
     './.storybook/**/*.{js,jsx,ts,tsx}', // Storybook 설정 경로
   ],
   theme: {
-    extend: {},
-    screens: {
-      sm: '360px',
-      // => @media (min-width: 360px) { ... }
+    extend: {
+      screens: {
+        sm: '360px',
+        // => @media (min-width: 360px) { ... }
 
-      md: '744px',
-      // => @media (min-width: 744px) { ... }
+        md: '744px',
+        // => @media (min-width: 744px) { ... }
 
-      lg: '1200px',
-      // => @media (min-width: 1200px) { ... }
+        lg: '1200px',
+        // => @media (min-width: 1200px) { ... }
+      },
+      colors: {
+        gray: {
+          50: '#F9FAFB',
+          100: '#F3F4F6',
+          200: '#E5E7EB',
+          300: '#D1D5DB',
+          400: '#9CA3AF',
+          500: '#6B7280',
+          600: '#4B5563',
+          700: '#374151',
+          800: '#1F2937',
+          900: '#111827',
+          950: '#030712',
+        },
+      },
     },
   },
   plugins: [],
