33aa746282cfa721ad56de2ffa71cc54f1a49dbd
HaeJungg <gpwnd0120@gmail.com>
2024-10-17 18:29:51 +0900

    🎨 style: 컴포넌트 삭제

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import category from '@/src/data/category.json';
-import InternalCategory from '@/src/components/common/category/internal-category';
-
 export default function Home() {
-  return (
-    <div className="container">
-      <InternalCategory items={category[0].items} />
-    </div>
-  );
+  return <div className="container">CREW CREW</div>;
 }
