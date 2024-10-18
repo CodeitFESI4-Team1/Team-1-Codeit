9b3d74faa3ae6722ab539c4441d2695b9826091b
HaeJungg <gpwnd0120@gmail.com>
2024-10-18 13:57:58 +0900

    🚧 chore: prettier changed

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,6 @@ import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import '@mantine/core/styles.css';
-
 import './globals.css';
 import { theme } from './theme';
 
