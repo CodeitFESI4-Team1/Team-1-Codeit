43b4e70b14003d4b696acb286d2a1e52d30b40cb
HaeJungg <gpwnd0120@gmail.com>
2024-10-18 09:46:34 +0900

    👷 chore: 프리티어 설정 변경, 변경된 import

--- src/components/common/input/drop-down.stories.tsx
+++ src/components/common/input/drop-down.stories.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react';
 import { ComboboxItem } from '@mantine/core';
 import { action } from '@storybook/addon-actions';
 import type { Meta, StoryFn } from '@storybook/react';
-import DropDown, { DropDownProps } from './drop-down';
+import DropDown, { DropDownProps } from './DropDown';
 
 const meta: Meta = {
   title: 'Components/DropDown',
