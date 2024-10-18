a0bca8e9581106f4e66507487871c950ca8aea2a
User850413 <csyou1228@gmail.com>
2024-10-18 13:40:53 +0900

    :lipstick: feat: reviewCard 諛곗튂

--- src/components/common/review-list/review-card-list.tsx
+++ src/components/common/review-list/review-card-list.tsx
@@ -1,8 +1,4 @@
-import ReviewCard, { ReviewCardProps } from './review-card';
-
-export interface ReviewCardListProps {
-  reviewList: ReviewCardProps[];
-}
+import ReviewCard, { ReviewCardListProps } from './review-card';
 
 // 리뷰 리스트[]를 prop으로 받아 리뷰 카드에 정보를 넘겨준 후 리스트 형식으로 렌더
 export default function ReviewCardList({ reviewList }: ReviewCardListProps) {
