f7223ed725a854109963e46b379d326fd125ace3
HaeJungg <gpwnd0120@gmail.com>
2024-10-18 09:46:34 +0900

    👷 chore: 프리티어 설정 변경, 변경된 import

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,7 +3,8 @@ import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import '@mantine/core/styles.css';
-import './globals.css';
+
+import '../styles/globals.css';
 import { theme } from './theme';
 
 export const metadata: Metadata = {
--- src/components/common/profile/index.tsx
+++ src/components/common/profile/index.tsx
@@ -1,8 +1,9 @@
 'use client';
 
+import Image from 'next/image';
+
 import defaultImage from '@/public/assets/icons/default-profile.svg';
 import editImage from '@/public/assets/icons/profile-edit.svg';
-import Image from 'next/image';
 
 /**
  * Profile 컴포넌트
