df7c7715736276a5db3b3e4bec73720a259759f6
Lee Youl <2yulrang@gmail.com>
2024-10-18 10:08:38 +0900

    🚧 Chore: import order 문제 수정

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,10 +1,8 @@
 import type { Metadata } from 'next';
-
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import '@mantine/core/styles.css';
-
 import './globals.css';
 import { theme } from './theme';
 
