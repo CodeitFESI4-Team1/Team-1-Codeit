26f312895e04dd6c1875a23b4dc4ca4b83d79aa3
HaeJungg <gpwnd0120@gmail.com>
2024-10-22 19:05:50 +0900

    ✨ feat: 모임 카드 컴포넌트 추가

---
+++ src/components/common/gathering-card/container.tsx
@@ -0,0 +1,53 @@
+'use client';
+
+import { useState } from 'react';
+import GatheringCardPresenter from './presenter';
+
+interface GatheringCardContainerProps {
+  title: string;
+  date: string;
+  location: string;
+  currentCount: number;
+  totalCount: number;
+  imageUrl: string;
+  isLiked: boolean;
+  // onLikeToggle: () => void;
+}
+
+export default function GatheringCard({
+  title,
+  date,
+  location,
+  currentCount,
+  totalCount,
+  imageUrl,
+  isLiked: initialIsLiked,
+}: GatheringCardContainerProps) {
+  // 날짜 비교
+  const gatheringDate = new Date(date);
+  const today = new Date();
+  const isPast = gatheringDate < today;
+
+  // 임시 찜하기
+  const [isLiked, setIsLiked] = useState(initialIsLiked);
+
+  // 추후 찜하기 컴포넌트 작성되면 수정
+  const handleLikeToggle = () => {
+    setIsLiked((prev) => !prev);
+  };
+
+  return (
+    <GatheringCardPresenter
+      isPast={isPast}
+      title={title}
+      date={date}
+      location={location}
+      currentCount={currentCount}
+      totalCount={totalCount}
+      imageUrl={imageUrl}
+      isLiked={isLiked}
+      onLikeToggle={handleLikeToggle}
+      onClick={() => alert('카드클릭!')}
+    />
+  );
+}
