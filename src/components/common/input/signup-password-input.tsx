74f2e91e6506253e30315ee1ee960bc665163fc8
min5x5 <minkyung5x5@gmail.com>
2024-10-22 17:28:57 +0900

    ✨ SignupPasswordInput requirements 수정, 🎨 style: bg color, border 수정

--- src/components/common/input/signup-password-input.tsx
+++ src/components/common/input/signup-password-input.tsx
@@ -1,97 +1,38 @@
 'use client';
 
-import { Box, Center, Group, PasswordInput, Progress, Text } from '@mantine/core';
+import { PasswordInput } from '@mantine/core';
 import { useInputState } from '@mantine/hooks';
-import { IconCheck, IconX } from '@tabler/icons-react';
-import { v4 as uuidv4 } from 'uuid';
 
-const requirements = [
-  { re: /.{6,}/, label: 'Has at least 6 characters' },
-  { re: /[0-9]/, label: 'Includes number' },
-  { re: /[a-z]/, label: 'Includes lowercase letter' },
-  { re: /[A-Z]/, label: 'Includes uppercase letter' },
-  { re: /[$&+,:;=?@#|'<>.^*()%!-]/, label: 'Includes special symbol' },
-];
-
-const requirementsCount = requirements.length;
-
-const getStrength = (password: string) => {
-  let multiplier = 0;
-
-  requirements.forEach((requirement) => {
-    if (!requirement.re.test(password)) {
-      multiplier += 1;
-    }
-  });
-
-  return Math.max(100 - (100 / requirements.length) * multiplier, 0);
-};
-
-const getProgressValue = (value: string, index: number, strength: number) => {
-  if (value.length > 0 && index === 0) {
-    return 100;
-  }
-  if (strength >= ((index + 1) / requirementsCount) * 100) {
-    return 100;
-  }
-  return 0;
-};
-
-const getColorByStrength = (strength: number) => {
-  if (strength > 80) {
-    return 'teal';
-  }
-  if (strength > 50) {
-    return 'yellow';
-  }
-  return 'red';
+const passwordInputConfig = {
+  label: '비밀번호',
+  placeholder: '비밀번호를 입력해주세요',
+  requirement: { re: /.{8,}/, label: '비밀번호가 8자 이상이 되도록 입력해주세요.' },
 };
 
 export default function SignupPasswordInput() {
   const [value, setValue] = useInputState('');
-  const strength = getStrength(value);
+
+  const isPasswordInvalid = (passwordInputValue: string) => {
+    if (value.length > 0 && !passwordInputConfig.requirement.re.test(passwordInputValue)) {
+      return true;
+    }
+    return false;
+  };
+
   return (
     <div>
       <PasswordInput
         value={value}
         onChange={setValue}
-        placeholder="Your password"
-        label="Password"
+        placeholder={passwordInputConfig.placeholder}
+        label={passwordInputConfig.label}
+        labelProps={{ className: 'mb-2' }}
+        error={isPasswordInvalid(value) ? passwordInputConfig.requirement.label : ''}
+        errorProps={{ className: 'mt-2 text-sm' }}
+        classNames={{ input: `bg-gray-100 ${isPasswordInvalid(value) ? 'border-2' : 'border-0'} ` }}
+        radius="md"
         required
       />
-
-      <Group gap={5} grow mt="xs" mb="md">
-        {Array(requirementsCount)
-          .fill(0)
-          .map((_, index) => (
-            <Progress
-              styles={{ section: { transitionDuration: '0ms' } }}
-              value={getProgressValue(value, index, strength)}
-              color={getColorByStrength(strength)}
-              key={uuidv4()}
-              size={requirementsCount}
-            />
-          ))}
-      </Group>
-
-      {requirements.map((requirement) => (
-        <Text
-          key={uuidv4()}
-          component="div"
-          c={requirement.re.test(value) ? 'teal' : 'red'}
-          mt={5}
-          size="sm"
-        >
-          <Center inline>
-            {requirement.re.test(value) ? (
-              <IconCheck size="0.9rem" stroke={1.5} />
-            ) : (
-              <IconX size="0.9rem" stroke={1.5} />
-            )}
-            <Box ml={7}>{requirement.label}</Box>
-          </Center>
-        </Text>
-      ))}
     </div>
   );
 }
