8e0fa8aee814c06230d37ac17ec22cea4a412d61
User850413 <csyou1228@gmail.com>
2024-10-23 15:14:40 +0900

    test_tube: test: review-card-list.stories 파일 수정

--- src/components/common/review-list/review-card-list.tsx
+++ src/components/common/review-list/review-card-list.tsx
@@ -1,11 +1,10 @@
-import { ReviewList } from '@/src/types/ReviewList';
-import ReviewCard from './review-card';
+import ReviewCard, { ReviewList } from './review-card';
 
 /**
  * ReviewCardList 컴포넌트
  *
  * @param {ReviewList} reviewList - 리뷰 리스트 데이터 배열
- * @param {boolean} [props.clickable] - 클릭가능여부, 클릭 시 해당 모임이 포함된 크루 페이지로 이동, 기본값 'true'
+ * @param {boolean} [props.clickable] - 클릭가능여부, 클릭 시 해당 모임이 포함된 크루 페이지로 이동, 기본값 'false'
  * @param {boolean} [props.imageAvailable] - 썸네일 이미지 유무, 기본값 'true'
  * @returns {JSX.Element} : ReviewCardList
  */
@@ -19,8 +18,8 @@ interface ReviewCardListProps {
 // 리뷰 리스트[]를 prop으로 받아 리뷰 카드에 정보를 넘겨준 후 리스트 형식으로 렌더
 export default function ReviewCardList({
   reviewList,
-  clickable = true,
-  imageAvailable = true,
+  clickable = false,
+  imageAvailable = false,
 }: ReviewCardListProps) {
   return (
     <ul className="mx-auto w-[343px] md:w-[820px] lg:w-[1200px]">
@@ -31,6 +30,9 @@ export default function ReviewCardList({
             comment={review.comment}
             createdAt={review.createdAt}
             user={review.user}
+            imageAvailable={imageAvailable}
+            clickable={clickable}
+            gatheringId={review.gathering.id}
           />
         </li>
       ))}
