7875412e2e2605ac876d8949b3f5014656a963ec
youl <2yulrang@gmail.com>
2024-10-23 11:01:58 +0900

    💄 Design: 디자인에 맞게 스타일 수정

--- src/components/common/input/drop-down.tsx
+++ src/components/common/input/drop-down.tsx
@@ -1,3 +1,4 @@
+import { useState } from 'react';
 import Image from 'next/image';
 import { ComboboxItem, Select } from '@mantine/core';
 import IcoArrowDown from '@/public/assets/icon/ic-arrow-down.svg';
@@ -29,30 +30,66 @@ export default function DropDown({
   placeholder,
   className,
 }: DropDownProps) {
+  const [theme, setTheme] = useState('white');
+
   return (
     <Select
       mt="md"
       data={data}
       value={value}
+      onFocus={() => setTheme('dark')}
+      onBlur={() => setTheme('white')}
       onChange={onChange}
       withCheckIcon={false}
       leftSection={
         variant === 'sort' && (
-          <Image src={IcoSort} width={24} height={24} alt="아이콘" aria-hidden="true" />
+          <svg
+            width="24"
+            height="24"
+            aria-hidden="true"
+            className="group-focus:fill-white"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M3 11L7 7M7 7L11 11M7 7V17"
+              stroke={`${theme === 'dark' ? '#ffffff' : '#D1D5DB'}`}
+              stroke-width="1.8"
+              stroke-linecap="round"
+            />
+            <path
+              d="M21 13L17 17M17 17L13 13M17 17V7"
+              stroke={`${theme === 'dark' ? '#ffffff' : '#D1D5DB'}`}
+              stroke-width="1.8"
+              stroke-linecap="round"
+            />
+          </svg>
         )
       }
       rightSection={
         variant !== 'sort' && (
-          <Image src={IcoArrowDown} width={24} height={24} alt="아이콘" aria-hidden="true" />
+          <svg
+            width="24"
+            height="24"
+            viewBox="0 0 24 24"
+            fill="none"
+            xmlns="http://www.w3.org/2000/svg"
+          >
+            <path
+              d="M12.7151 15.4653C12.3975 15.7654 11.9008 15.7654 11.5832 15.4653L5.8047 10.006C5.26275 9.49404 5.6251 8.58286 6.37066 8.58286L17.9276 8.58286C18.6732 8.58286 19.0355 9.49404 18.4936 10.006L12.7151 15.4653Z"
+              fill={`${theme === 'dark' ? '#ffffff' : '#D1D5DB'}`}
+            />
+          </svg>
         )
       }
       placeholder={placeholder}
       classNames={{
         wrapper: `${className}`,
-        input: `rounded-xl border-0 py-2 px-3 placeholder-gray-800 ${variant === 'sort' && 'pl-9'}`,
+        input: `focus:bg-black focus:placeholder:text-white focus:text-white peer rounded-xl border-0 py-2 px-3 placeholder-gray-800 ${variant === 'sort' && 'pl-9'}`,
         section: `end-1 ${variant === 'sort' && 'data-[position=right]:hidden start-1'}`,
-        dropdown: 'rounded-xl shadow-xl p-0 border-0',
-        option: `py-2.5 px-3 text-gray-800 ${variant === 'sort' && 'justify-center'}`,
+        dropdown: 'rounded-xl shadow-xl p-0 border-0 mt-2',
+        option: `py-2.5 m-1 px-3 rounded-xl text-gray-800 hover:bg-blue-100 ${variant === 'sort' && 'justify-center'}`,
       }}
       comboboxProps={{ position: 'bottom', middlewares: { flip: false, shift: false }, offset: 0 }}
     />
--- tailwind.config.ts
+++ tailwind.config.ts
@@ -33,7 +33,7 @@ const config: Config = {
         },
       },
       boxShadow: {
-        xl: '0 10px 10px -5px rgba(0,0,0,0.04)',
+        xl: '0 4px 4px 0 rgba(0,0,0,0.25)',
       },
     },
   },
