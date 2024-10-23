9f6fd0c78877b24ec291addb5d71831165c74674
Lee Youl <2yulrang@gmail.com>
2024-10-23 17:31:00 +0900

    ♻️ Refactor: dropdown data 형식 변경

--- src/components/common/input/drop-down/index.tsx
+++ src/components/common/input/drop-down/index.tsx
@@ -1,5 +1,5 @@
 import { useState } from 'react';
-import { ComboboxItem, Select } from '@mantine/core';
+import { ComboboxData, ComboboxItem, Select } from '@mantine/core';
 
 /**
  * drop-down Component
@@ -12,10 +12,10 @@ import { ComboboxItem, Select } from '@mantine/core';
 
 export interface DropDownProps {
   variant: 'region' | 'category' | 'sort';
-  data: string[];
+  data: ComboboxData | undefined;
   placeholder: string;
   value: string | null;
-  onChange: (_value: string | null, option: ComboboxItem) => void;
+  onChange: (newValue: string | null, option: ComboboxItem) => void;
   className: string;
 }
 
@@ -83,7 +83,7 @@ export default function DropDown({
       placeholder={placeholder}
       classNames={{
         wrapper: `${className}`,
-        input: `focus:bg-black focus:placeholder:text-white focus:text-white typo-base-medium rounded-xl border-0 py-2 px-3 placeholder-gray-800 ${variant === 'sort' && 'pl-9'}`,
+        input: `focus:bg-black focus:placeholder:text-white focus:text-white typo-base-medium rounded-xl border-0 h-11 py-2.5 px-3 placeholder-gray-800 ${variant === 'sort' && 'pl-9'}`,
         section: `end-1 ${variant === 'sort' && 'data-[position=right]:hidden start-1'}`,
         dropdown: 'rounded-xl shadow-xl p-0 border-0 mt-2',
         option: `py-1 m-1 px-2 mr-0 rounded-xl text-gray-800 typo-base-medium hover:bg-blue-100 ${variant === 'sort' && 'justify-center'}`,
