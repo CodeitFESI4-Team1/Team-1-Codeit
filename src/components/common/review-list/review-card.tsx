90c8da21aad616f26b8c96b26a1947b69ff23b15
User850413 <csyou1228@gmail.com>
2024-10-23 15:16:00 +0900

    :bug: fix: type 관련 오류 수정

--- src/components/common/review-list/review-card.tsx
+++ src/components/common/review-list/review-card.tsx
@@ -1,5 +1,38 @@
-import { User } from '@/src/types/ReviewList';
+'use client';
+
+import { useState } from 'react';
+import Image from 'next/image';
+import { useRouter } from 'next/router';
 import Heart from '@/public/assets/icon/ic-heart';
+
+export type Gathering = {
+  teamId: number;
+  id: number;
+  type: string;
+  name: string;
+  dateTime: Date;
+  location: string;
+  image: string;
+};
+
+export type User = {
+  teamId: number;
+  id: number;
+  name: string;
+  image: string;
+};
+
+export type Review = {
+  teamId: number;
+  id: number;
+  score: number;
+  comment: string;
+  createdAt: Date;
+  gathering: Gathering;
+  user: User;
+};
+
+export type ReviewList = Review[];
 
 /**
  * ReviewCard 컴포넌트
@@ -8,6 +41,8 @@ import Heart from '@/public/assets/icon/ic-heart';
  * @param {string} [props.comment] - 리뷰 내용
  * @param {Date} [props.createdAt] - 게시 일시
  * @param {User} [props.user] - 게시인
+ * @param {boolean} [props.imageAvailable] - 썸네일 이미지 유무, 기본값 'false'
+ * @param {boolean} [props.clickable] - 클릭 가능한지, 기본값 'false'
  * @returns {JSX.Element} - ReviewCard
  */
 
@@ -16,6 +51,9 @@ interface ReviewCardProps {
   comment: string;
   createdAt: Date;
   user: User;
+  imageAvailable?: boolean;
+  clickable?: boolean;
+  gatheringId: number;
 }
 
 // NOTE: 추후 reviewHeart 컴포넌트로 교체
@@ -31,7 +69,29 @@ function MockScore({ score }: { score: number }) {
   );
 }
 
-export default function ReviewCard({ score, comment, createdAt, user }: ReviewCardProps) {
+export default function ReviewCard({
+  score,
+  comment,
+  createdAt,
+  user,
+  imageAvailable = false,
+  clickable = false,
+  gatheringId,
+}: ReviewCardProps) {
+  const [prefetched, setPrefetched] = useState(new Set());
+  const CREWPAGE = `/detail/${gatheringId}`;
+  const router = useRouter();
+
+  const handleClick = () => {
+    if (clickable) router.push(CREWPAGE);
+  };
+
+  const handlePrefetch = () => {
+    if (clickable && !prefetched.has(CREWPAGE)) router.prefetch(CREWPAGE);
+    setPrefetched(new Set(prefetched).add(CREWPAGE));
+  };
+
+  // NOTE: Date를 YYYY.MM.DD 형식으로 변환
   const formatDate = (date: Date) => {
     const yyyy = date.getFullYear();
     const mm = String(date.getMonth() + 1).padStart(2, '0');
@@ -43,12 +103,17 @@ export default function ReviewCard({ score, comment, createdAt, user }: ReviewCa
   const reviewDate = formatDate(createdAt);
 
   return (
-    <div className="flex w-full flex-col items-start border-[2px] border-gray-400 px-[60px] py-4">
+    <div
+      role="presentation"
+      onClick={handleClick}
+      onMouseEnter={handlePrefetch}
+      className={`flex w-full flex-col items-start border-[2px] border-gray-400 px-[60px] py-4 ${clickable ? 'cursor-pointer' : 'cursor-default'}`}
+    >
       <MockScore score={score} />
       <p className="mb-2 mt-2.5 text-sm font-medium">{comment}</p>
       <div className="flex items-center text-xs">
-        <span className="h-6 w-6 rounded-full bg-red-500">
-          <img src={user.image} alt={user.name} />
+        <span className="relative h-6 w-6 rounded-full bg-red-500">
+          <Image src={user.image} alt={user.name} fill />
         </span>
         <span className="relative block w-fit px-2 after:absolute after:right-0 after:content-['|']">
           {user.name}
