f8f046a1504cd4f1ae976a83c46664ed58e3ddae
HaeJungg <gpwnd0120@gmail.com>
2024-10-23 16:31:37 +0900

    📝 docs: 모임 카드 목데이터 추가

---
+++ src/mock/gathering-data.ts
@@ -0,0 +1,162 @@
+export const gatheringData = [
+  {
+    title: '가나다라마가나다라마가나다라마가',
+    date: '2024-12-15T07:30',
+    location: '서울, 한강공원',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1601758260892-a62c486ace97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    isLiked: true,
+  },
+  {
+    title: '등산 모임',
+    date: '2024-11-12T09:00',
+    location: '서울, 한강공원 길어질 경우 처리리리리리',
+    currentCount: 5,
+    totalCount: 10,
+    imageUrl:
+      'https://images.unsplash.com/photo-1516978101789-720eacb59e79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fGNhdHxlbnwwfHwwfHx8Mg%3D%3D',
+    isLiked: false,
+  },
+  {
+    title: '가나다라 모임',
+    date: '2024-10-15T07:30',
+    location: '서울, 한강공원',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1516978101789-720eacb59e79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fGNhdHxlbnwwfHwwfHx8Mg%3D%3D',
+    isLiked: true,
+  },
+  {
+    title: '조깅 모임',
+    date: '2024-12-15T07:30',
+    location: '서울, 한강공원',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1516978101789-720eacb59e79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fGNhdHxlbnwwfHwwfHx8Mg%3D%3D',
+    isLiked: false,
+  },
+  {
+    title: '조깅 모임',
+    date: '2024-12-15T07:30',
+    location: '서울, 한강공원',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1516978101789-720eacb59e79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fGNhdHxlbnwwfHwwfHx8Mg%3D%3D',
+    isLiked: true,
+  },
+  {
+    title: '등산 모임',
+    date: '2024-11-12T09:00',
+    location: '서울, 북한산',
+    currentCount: 5,
+    totalCount: 10,
+    imageUrl:
+      'https://images.unsplash.com/photo-1601758260892-a62c486ace97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    isLiked: false,
+  },
+  {
+    title: '가나다라 모임',
+    date: '2024-10-15T07:30',
+    location: '서울, 한강공원',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1516978101789-720eacb59e79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fGNhdHxlbnwwfHwwfHx8Mg%3D%3D',
+    isLiked: true,
+  },
+  {
+    title: '조깅 모임',
+    date: '2024-12-15T07:30',
+    location: '서울, 한강공원',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1516978101789-720eacb59e79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fGNhdHxlbnwwfHwwfHx8Mg%3D%3D',
+    isLiked: false,
+  },
+  {
+    title: '조깅 모임',
+    date: '2024-12-15T07:30',
+    location: '서울, 한강공원',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1516978101789-720eacb59e79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fGNhdHxlbnwwfHwwfHx8Mg%3D%3D',
+    isLiked: true,
+  },
+  {
+    title: '등산 모임',
+    date: '2024-11-12T09:00',
+    location: '서울, 북한산',
+    currentCount: 5,
+    totalCount: 10,
+    imageUrl:
+      'https://images.unsplash.com/photo-1601758260892-a62c486ace97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    isLiked: false,
+  },
+  {
+    title: '가나다라 모임',
+    date: '2024-10-15T07:30',
+    location: '서울, 한강공원',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1601758260892-a62c486ace97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    isLiked: true,
+  },
+  {
+    title: '조깅 모임',
+    date: '2024-12-15T07:30',
+    location: '서울, 한강공원',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1601758260892-a62c486ace97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    isLiked: false,
+  },
+  {
+    title: '조깅 모임',
+    date: '2024-12-15T07:30',
+    location: '서울, 한강공원',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1516978101789-720eacb59e79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fGNhdHxlbnwwfHwwfHx8Mg%3D%3D',
+    isLiked: true,
+  },
+  {
+    title: '등산 모임',
+    date: '2024-11-12T09:00',
+    location: '서울, 북한산',
+    currentCount: 5,
+    totalCount: 10,
+    imageUrl:
+      'https://images.unsplash.com/photo-1516978101789-720eacb59e79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fGNhdHxlbnwwfHwwfHx8Mg%3D%3D',
+    isLiked: false,
+  },
+  {
+    title: '가나다라 모임',
+    date: '2024-10-15T07:30',
+    location: '서울, 한강공원',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1601758260892-a62c486ace97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+    isLiked: true,
+  },
+  {
+    title: '가나다라마가나다라마가나다라마가',
+    date: '2024-12-15T07:30',
+    location: '서울, 한강공원 길어질 경우 처리리리리리',
+    currentCount: 8,
+    totalCount: 12,
+    imageUrl:
+      'https://images.unsplash.com/photo-1516978101789-720eacb59e79?w=500&auto=format&fit=crop&q=60&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxzZWFyY2h8NjV8fGNhdHxlbnwwfHwwfHx8Mg%3D%3D',
+    isLiked: false,
+  },
+];
