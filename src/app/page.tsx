b414bb327257ce5a798df47e1392f5d1e3594c71
HaeJungg <gpwnd0120@gmail.com>
2024-10-24 10:38:04 +0900

    🎨 design: 페이지 스타일 추가

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,7 +2,7 @@ import GatheringCardCarousel from '../components/gathering-list/gathering-card-c
 
 export default function Home() {
   return (
-    <div className="container mx-auto w-full">
+    <div className="container max-w-[1200px] mx-auto my-0">
       CREW CREW
       <h1 className="my-8 text-center text-2xl font-bold">Gathering Carousel</h1>
       <GatheringCardCarousel />
