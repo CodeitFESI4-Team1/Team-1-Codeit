733a12d6d32c216c2de4d822bfd1204243a80567
HaeJungg <gpwnd0120@gmail.com>
2024-10-24 14:41:49 +0900

    🚧 wip: 이전으로 되돌림

--- src/app/api/gatherings/route.ts
+++
@@ -1,6 +0,0 @@
-import { NextResponse } from 'next/server';
-import { gatheringData } from '@/src/mock/gathering-data';
-
-export async function GET() {
-  return NextResponse.json(gatheringData);
-}
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,3 @@
-import GatheringCardCarousel from '../components/gathering-list/gathering-card-carousel';
-
 export default function Home() {
-  return (
-    <div className="container max-w-[1200px] mx-auto my-0">
-      CREW CREW
-      <h1 className="my-8 text-center text-2xl font-bold">Gathering Carousel</h1>
-      <GatheringCardCarousel />
-    </div>
-  );
+  return <div className="container max-w-[1200px] mx-auto my-0">CREW CREW</div>;
 }
