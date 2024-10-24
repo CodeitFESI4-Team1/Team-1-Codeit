11e1f00a3219f02801b341d0dc747b74b0706a77
Lee Youl <2yulrang@gmail.com>
2024-10-24 16:44:23 +0900

    💄 Design : 반응형 스타일 적용

--- src/app/my-gathering/page.tsx
+++ src/app/my-gathering/page.tsx
@@ -8,5 +8,9 @@ const toDoDates = [new Date('2024-10-12'), new Date('2024-10-15')];
 export default function MyGatheringPage() {
   const [date, setDate] = useState<Date>(new Date());
 
-  return <CalendarFilter value={date} toDoDates={toDoDates} onChange={setDate} />;
+  return (
+    <div className="container max-w-[1200px] mx-auto my-0 px-5 lg:px-0">
+      <CalendarFilter value={date} toDoDates={toDoDates} onChange={setDate} />
+    </div>
+  );
 }
