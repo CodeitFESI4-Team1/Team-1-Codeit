7eedec7eb81ed3b3c967ad26187904c20e08133e
Lee Youl <2yulrang@gmail.com>
2024-10-24 10:33:48 +0900

    💄 Design: 가운데 정렬 수정

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -8,7 +8,7 @@ export default function Home() {
   const [sort, setSort] = useState<string | null>('latest');
 
   return (
-    <div className="container max-w-[1200px] my-auto mx-0">
+    <div className="container max-w-[1200px] mx-auto my-0">
       <CategoryWrap />
       <DropDown
         variant="sort"
