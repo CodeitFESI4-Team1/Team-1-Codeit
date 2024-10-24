c019d2276fa7e00eb0e4f4ed6396f7144186b65f
조예은 <yeeunee73@gmail.com>
2024-10-24 14:39:32 +0900

    ✨ Feat: modal container
    테스트 후 스토리 파일은 에러 땜에 잠시 지움

---
+++ src/components/common/modal/modal-container.tsx
@@ -0,0 +1,21 @@
+'use client';
+
+import React, { useState } from 'react';
+import { Modal } from '@mantine/core';
+
+export default function ModalContainer({ children }: { children: React.ReactNode }) {
+  const [isOpen, setIsOpen] = useState(true);
+
+  return (
+    <Modal
+      opened={isOpen}
+      onClose={() => setIsOpen(false)}
+      centered
+      withCloseButton={false}
+      size="auto"
+      overlayProps={{ backgroundOpacity: 0.5 }}
+    >
+      {children}
+    </Modal>
+  );
+}
