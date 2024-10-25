8b13caea8f91ff00a131feea31725394db9f9686
min5x5 <minkyung5x5@gmail.com>
2024-10-25 15:43:54 +0900

    ♻️ refactor: text input로 합쳐서 수정 및 리팩토링

---
+++ src/components/common/input/text-input/text-input.stories.tsx
@@ -0,0 +1,35 @@
+import React from 'react';
+import type { Meta, StoryFn } from '@storybook/react';
+import TextInput, { TextInputProps } from '.';
+
+const meta: Meta = {
+  title: 'Components/input/text-input',
+  component: TextInput,
+};
+
+export default meta;
+const Template: StoryFn<TextInputProps> = function TextInputStory(args: TextInputProps) {
+  return <TextInput {...args} />;
+};
+
+export const Default = Template.bind({});
+Default.args = {
+  placeholder: '검색어를 입력하세요',
+  type: 'text',
+  inputClassNames: 'bg-gray-100',
+};
+
+export const WithLabel = Template.bind({});
+WithLabel.args = {
+  label: '이메일',
+  placeholder: '이메일을 입력하세요',
+  type: 'email',
+  inputClassNames: 'bg-gray-100',
+};
+
+export const WithError = Template.bind({});
+WithError.args = {
+  error: 'Invalid email format',
+  type: 'text',
+  inputClassNames: 'bg-gray-100',
+};
