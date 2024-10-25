762b00a64a6a1cc3194680fbc7e3d77e169c5a54
Lee Youl <2yulrang@gmail.com>
2024-10-25 14:10:12 +0900

    🚚 Chore: 파일 이동

--- src/app/detail/page.tsx
+++ src/app/detail/page.tsx
@@ -6,7 +6,7 @@ import DateTimePicker from '@/src/components/common/input/date-time-picker';
 export default function CrewDetailPage() {
   const [fullDate, setFullDate] = useState(new Date());
   return (
-    <div className="container max-w-[1200px] mx-auto my-0 px-5 lg:px-0">
+    <div className="container mx-auto my-0 max-w-pc px-5 lg:px-0">
       <DateTimePicker fullDate={fullDate} onChange={setFullDate} />
     </div>
   );
