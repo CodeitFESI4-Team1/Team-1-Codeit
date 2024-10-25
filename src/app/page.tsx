1ecbb0c8c1268cec583fbd7b189e23abee27ae16
HaeJungg <gpwnd0120@gmail.com>
2024-10-25 15:48:26 +0900

    🚧 wip: 이전으로 되돌림

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,9 @@
+import CategoryWrap from '@/src/components/common/category/category-wrap';
+
 export default function Home() {
-  return <div className="container mx-auto my-0 max-w-[1200px]">CREW CREW</div>;
+  return (
+    <div className="container mx-auto my-0 max-w-[1200px]">
+      <CategoryWrap />
+    </div>
+  );
 }
