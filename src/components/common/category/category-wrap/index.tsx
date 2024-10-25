c1ce5d44494e92e9ce6163fffb23c05403d17959
Lee Youl <2yulrang@gmail.com>
2024-10-25 11:42:50 +0900

    🎨 Style: 프리티어 적용

--- src/components/common/category/category-wrap/index.tsx
+++ src/components/common/category/category-wrap/index.tsx
@@ -9,7 +9,7 @@ export default function CategoryWrap() {
   const [categoryIndex, setCategoryIndex] = useState(0);
 
   return (
-    <div className="lg:px-7.5 flex flex-col gap-6 px-2.5">
+    <div className="flex flex-col gap-6 px-2.5 lg:px-7.5">
       <MainCategory data={category} onHover={setCategoryIndex} />
       <InternalCategory items={category[categoryIndex].items} />
     </div>
