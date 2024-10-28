c89fbfd34c038dcbc4b767c7d0b9f485bb8cb314
min5x5 <minkyung5x5@gmail.com>
2024-10-28 09:47:58 +0900

    📝 Docs: PasswordInput Props 설명 추가

--- src/components/common/input/password-input/index.tsx
+++ src/components/common/input/password-input/index.tsx
@@ -5,8 +5,16 @@ import {
   PasswordInputProps as MantinePasswordInputProps,
 } from '@mantine/core';
 
+/**
+ * PasswordInput
+ *
+ * @param {string} placeholder - 입력 필드의 플레이스홀더 텍스트
+ * @param {string | null} [error] - 유효성 검사 실패 시 표시되는 에러 메시지 (옵션, 없을 시 null)
+ * @param {UseFormRegisterReturn} register - react-hook-form의 register 함수 반환값으로, 입력 필드를 폼에 등록하기 위해 사용
+ * @param {object} rest - Mantine의 PasswordInput 컴포넌트에 전달할 나머지 props (예: onChange, onBlur 등)
+ */
+
 export interface PasswordInputProps extends MantinePasswordInputProps {
-  label: string;
   placeholder: string;
   error?: string | null;
   register: UseFormRegisterReturn;
@@ -20,7 +28,6 @@ export default function PasswordInput({
 }: PasswordInputProps) {
   return (
     <MantinePasswordInput
-      label={label}
       labelProps={{ className: 'mb-2' }}
       placeholder={error ? '' : placeholder}
       error={error}
