5dd4f61868f2cbda573104afeea268cecbd34aad
min5x5 <minkyung5x5@gmail.com>
2024-10-28 09:19:42 +0900

    📝 Docs: TextInput Props 설명 추가

--- src/components/common/input/text-input/index.tsx
+++ src/components/common/input/text-input/index.tsx
@@ -10,6 +10,16 @@ export interface TextInputProps extends MantineTextInputProps {
   inputClassNames?: string;
 }
 
+/**
+ * TextInput
+ *
+ * @param {string} placeholder - 입력 필드의 플레이스홀더 텍스트
+ * @param {string | null} error - 유효성 검사 실패 시 표시되는 에러 메시지 (없을 시 null)
+ * @param {UseFormRegisterReturn} register - react-hook-form의 register 함수 반환값, 입력 필드를 폼에 등록하기 위해 사용
+ * @param {string} [inputClassNames] - 입력 필드의 추가적인 커스텀 클래스
+ * @param {object} rest - Mantine의 TextInput 컴포넌트에 전달할 나머지 props
+ */
+
 export default function TextInput({
   placeholder,
   error,
