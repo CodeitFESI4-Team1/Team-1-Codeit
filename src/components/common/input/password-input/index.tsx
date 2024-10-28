a69dfaf337f4c58a9d27c59c8851fec858d2e0e2
min5x5 <minkyung5x5@gmail.com>
2024-10-28 11:21:31 +0900

    🐛 fix: PasswordInput input classnames 변수 추가

--- src/components/common/input/password-input/index.tsx
+++ src/components/common/input/password-input/index.tsx
@@ -9,20 +9,21 @@ import {
  * PasswordInput
  *
  * @param {string} placeholder - 입력 필드의 플레이스홀더 텍스트
- * @param {string | null} [error] - 유효성 검사 실패 시 표시되는 에러 메시지 (옵션, 없을 시 null)
- * @param {UseFormRegisterReturn} register - react-hook-form의 register 함수 반환값으로, 입력 필드를 폼에 등록하기 위해 사용
- * @param {object} rest - Mantine의 PasswordInput 컴포넌트에 전달할 나머지 props (예: onChange, onBlur 등)
+ * @param {string | null} error - 유효성 검사 실패 시 표시되는 에러 메시지 (없을 시 null)
+ * @param {UseFormRegisterReturn} register - react-hook-form의 register 함수 반환값, 입력 필드를 폼에 등록하기 위해 사용
+ * @param {string} [inputClassNames] - 입력 필드의 추가적인 커스텀 클래스
+ * @param {object} rest - Mantine의 TextInput 컴포넌트에 전달할 나머지 props
  */
 
 export interface PasswordInputProps extends MantinePasswordInputProps {
-  placeholder: string;
-  error?: string | null;
   register: UseFormRegisterReturn;
+  inputClassNames?: string;
 }
 export default function PasswordInput({
   placeholder,
   error,
   register,
+  inputClassNames,
   ...rest
 }: PasswordInputProps) {
   return (
@@ -31,7 +32,7 @@ export default function PasswordInput({
       placeholder={error ? '' : placeholder}
       error={error}
       errorProps={{ className: 'mt-2 text-sm' }}
-      classNames={{ input: `bg-gray-100 ${error ? 'border-2' : 'border-0'} ` }}
+      classNames={{ input: `${inputClassNames} ${error ? 'border-2' : 'border-0'} ` }}
       radius="md"
       {...register}
       {...rest}
