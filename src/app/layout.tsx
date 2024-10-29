b31a2de88b18a04d567a997da9675c4041274f0c
HaeJungg <gpwnd0120@gmail.com>
2024-10-29 13:41:22 +0900

    ✨ feat: add react query

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -3,6 +3,7 @@ import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 // Import styles of packages that you've installed.
 // All packages except `@mantine/hooks` require styles imports
 import '@mantine/core/styles.css';
+import ClientProvider from '@/src/components/common/client-provider';
 import '@/src/styles/globals.css';
 import { theme } from './theme';
 
@@ -22,11 +23,13 @@ export default function RootLayout({
         <ColorSchemeScript />
       </head>
       <body>
-        <MantineProvider theme={theme}>
-          {/* 헤더 컴포넌트 위치 */}
-          {/* <Header /> */}
-          {children}
-        </MantineProvider>
+        <ClientProvider>
+          <MantineProvider theme={theme}>
+            {/* 헤더 컴포넌트 위치 */}
+            {/* <Header /> */}
+            {children}
+          </MantineProvider>
+        </ClientProvider>
       </body>
     </html>
   );
