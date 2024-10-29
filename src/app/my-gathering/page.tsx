ab75dfeeaa76c4f6e8d2e8d0ed6b81ab677cbb83
Lee Youl <2yulrang@gmail.com>
2024-10-29 10:02:22 +0900

    🐛 Fix: 타입 수정

--- src/app/my-gathering/page.tsx
+++ src/app/my-gathering/page.tsx
@@ -6,7 +6,7 @@ import CalendarFilter from '@/src/components/common/input/calendar-filter';
 const toDoDates = [new Date('2024-10-12'), new Date('2024-10-15')];
 
 export default function MyGatheringPage() {
-  const [date, setDate] = useState<Date | undefined>(new Date());
+  const [date, setDate] = useState<Date>(new Date());
 
   return (
     <div className="container mx-auto my-0 max-w-pc px-5 lg:px-0">
