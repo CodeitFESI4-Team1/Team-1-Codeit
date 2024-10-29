1627d6fc2e34d2a579d32c3d898ace323f0868e1
Lee Youl <2yulrang@gmail.com>
2024-10-29 17:26:59 +0900

    💄 Design: 디자인 맞추기

--- src/components/common/category/main-category/index.tsx
+++ src/components/common/category/main-category/index.tsx
@@ -26,16 +26,16 @@ export default function MainCategory({ category, onHover }: MainCategoryProps) {
           >
             <Link
               href={item.title.href}
-              className={`${pathname?.includes(item.title.href) && 'bg-blue-500'} flex flex-col items-center text-sm font-semibold text-gray-600 hover:text-blue-500 md:flex-row md:text-lg lg:text-xl lg:font-semibold`}
+              className={`${pathname?.includes(item.title.href) && 'bg-blue-500'} group flex flex-col items-center text-sm font-semibold text-gray-600 hover:text-blue-500 md:flex-row md:text-lg lg:text-xl lg:font-semibold`}
             >
               <h3 className="flex items-center gap-1 lg:gap-2">
-                <span className="flex py-2.5 md:py-2">{item.title.label}</span>
+                <span className="flex py-3 md:py-2">{item.title.label}</span>
                 <Image
                   src={IcoDown}
                   width={40}
                   height={36}
                   alt="보기"
-                  className={`${pathname?.includes(item.title.href) ? 'flex' : 'hidden'} scale-75 group-hover:flex lg:scale-100`}
+                  className={`${pathname?.includes(item.title.href) && 'flex'} hidden scale-75 group-hover:flex lg:scale-100`}
                 />
               </h3>
             </Link>
