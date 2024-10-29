7dea7a8904c992ee65e9afaa7072bb50512b10f3
HaeJungg <gpwnd0120@gmail.com>
2024-10-29 16:46:05 +0900

    ✨ feat: ui에 맞게 format date 추가 및 변경

--- src/utils/format-date.ts
+++ src/utils/format-date.ts
@@ -1,15 +1,10 @@
 export function formatDate(dateString: string) {
   const date = new Date(dateString);
 
-  const year = String(date.getFullYear()).slice(2);
-  const month = String(date.getMonth() + 1).padStart(2, '0');
-  const day = String(date.getDate()).padStart(2, '0');
-
-  const hours = date.getHours();
+  const month = `${date.getMonth() + 1}월`;
+  const day = `${date.getDate()}일`;
+  const hours = String(date.getHours()).padStart(2, '0');
   const minutes = String(date.getMinutes()).padStart(2, '0');
 
-  const period = hours >= 12 ? 'PM' : 'AM';
-  const formattedHours = hours % 12 === 0 ? 12 : hours % 12;
-
-  return `${year}.${month}.${day} ${formattedHours}:${minutes}${period}`;
+  return { date: `${month} ${day}`, time: `${hours}:${minutes}` };
 }
