b876b0e5ac7727c7c8cdb56e49a925acd44d6df5
min5x5 <minkyung5x5@gmail.com>
2024-10-29 16:57:28 +0900

    👷 chore: json-server 설치

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,17 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  experimental: {
+    instrumentationHook: true,
+  },
+  webpack(config, { isServer }) {
+    if (isServer) {
+      config.resolve.alias['msw/browser'] = false;
+    } else {
+      config.resolve.alias['msw/node'] = false;
+    }
+
+    return config;
+  },
   images: {
     remotePatterns: [
       {
