5f5c2ae447d62135e04bc414b26b2d36c1a2cb71
min5x5 <minkyung5x5@gmail.com>
2024-10-29 16:58:30 +0900

    🐛 fix: config 설정 삭제

--- next.config.mjs
+++ next.config.mjs
@@ -1,17 +1,5 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  experimental: {
-    instrumentationHook: true,
-  },
-  webpack(config, { isServer }) {
-    if (isServer) {
-      config.resolve.alias['msw/browser'] = false;
-    } else {
-      config.resolve.alias['msw/node'] = false;
-    }
-
-    return config;
-  },
   images: {
     remotePatterns: [
       {
