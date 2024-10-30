510cb85032cf2ae9ca3de07f29ba9939ad322ce1
HaeJungg <gpwnd0120@gmail.com>
2024-10-30 14:39:52 +0900

    ✨ feat: 리뷰 탭 추가, 분기처리 추가

--- src/components/common/tab/index.tsx
+++ src/components/common/tab/index.tsx
@@ -7,32 +7,46 @@ export interface TabsProps {
   tabs: Tab[];
   activeTab: string;
   onTabClick: (id: string) => void;
+  variant?: 'default' | 'review';
 }
 
-export default function Tabs({ tabs, activeTab, onTabClick }: TabsProps) {
+export default function Tabs({ tabs, activeTab, onTabClick, variant = 'default' }: TabsProps) {
   const gap = 8;
   const tabCount = tabs.length;
   const buttonWidth = `calc((1200px - ${(tabCount - 1) * gap}px) / ${tabCount})`;
 
+  // 기본 스타일
+  const baseStyle =
+    variant === 'review'
+      ? 'flex h-11 w-32 items-center justify-center text-center text-base font-medium'
+      : 'px-4 py-2 font-bold sm:text-base md:text-lg lg:text-lg';
+
+  // 활성화된 탭 스타일
+  const activeStyle = variant === 'review' ? 'bg-black text-white' : 'bg-blue-500 text-white';
+
+  // 비활성화된 탭 스타일
+  const inactiveStyle =
+    variant === 'review' ? 'bg-white text-black' : 'border border-blue-500 bg-white text-blue-500';
+
   return (
     <div role="tablist" className="mx-auto flex max-w-[1200px] justify-center space-x-2">
-      {tabs.map((tab) => (
-        <button
-          key={tab.id}
-          role="tab"
-          type="button"
-          style={{ width: buttonWidth }}
-          aria-selected={activeTab === tab.id}
-          onClick={() => onTabClick(tab.id)}
-          className={`rounded-md px-4 py-2 font-bold sm:text-base md:text-lg lg:text-lg ${
-            activeTab === tab.id
-              ? 'bg-blue-500 text-white'
-              : 'border border-blue-500 bg-white text-blue-500'
-          }`}
-        >
-          {tab.label}
-        </button>
-      ))}
+      {tabs.map((tab) => {
+        const isActive = activeTab === tab.id;
+
+        return (
+          <button
+            key={tab.id}
+            role="tab"
+            type="button"
+            style={variant === 'default' ? { width: buttonWidth } : undefined}
+            aria-selected={isActive}
+            onClick={() => onTabClick(tab.id)}
+            className={`rounded-md ${baseStyle} ${isActive ? activeStyle : inactiveStyle}`}
+          >
+            {tab.label}
+          </button>
+        );
+      })}
     </div>
   );
 }
