660cc18c1fecaf81437e2b8059613fa0053c6f30
HaeJungg <gpwnd0120@gmail.com>
2024-10-30 17:49:53 +0900

    💄 design: 디자인 시안대로 수정

--- src/components/common/header/presenter.tsx
+++ src/components/common/header/presenter.tsx
@@ -1,9 +1,11 @@
 'use client';
 
+import Image from 'next/image';
 import Link from 'next/link';
 import { usePathname } from 'next/navigation';
 import { Menu } from '@mantine/core';
-import { Profile } from '../profile';
+import { Profile } from '@/src/components/common/profile';
+import Logo from '@/public/assets/images/logo.png';
 
 export interface HeaderPresenterProps {
   hasCookie: boolean;
@@ -18,40 +20,43 @@ export default function HeaderPresenter({
 }: HeaderPresenterProps) {
   const pathname = usePathname();
   return (
-    <header className="sticky top-0 z-10 w-full bg-purple-400 px-6 py-1">
-      <div className="mx-auto flex w-full max-w-screen-lg items-center justify-between">
+    <header className="sticky top-0 z-10 h-[40px] w-full bg-blue-500 px-6 md:h-[52px] lg:h-[52px]">
+      <div className="mx-auto flex h-full w-full max-w-screen-lg items-center justify-between">
         <div className="flex space-x-3 md:space-x-5 lg:space-x-5">
-          <h1 className="font-bold text-white">LOGO</h1>
+          <Link href="/" className="mt-1">
+            <Image src={Logo} alt="crew logo" width={83} height={30} className="hidden md:block" />
+            <Image src={Logo} alt="crew logo" width={53} height={28} className="block md:hidden" />
+          </Link>
           <nav className="flex items-center space-x-3 md:space-x-6 lg:space-x-6">
             <Link
               href="/"
-              className={`${pathname === '/' ? 'text-black' : 'text-white'} ml-4 font-bold`}
+              className={`${pathname === '/' ? 'text-white' : 'text-blue-300'} ml-4 hidden text-sm font-semibold md:block md:text-lg`}
             >
-              모임 찾기
+              크루 찾기
             </Link>
 
             {hasCookie ? (
               <>
                 <Link
                   href="/my-crew"
-                  className={`${pathname === '/my-crew' ? 'text-black' : 'text-white'} ml-4 font-bold`}
+                  className={`${pathname === '/my-crew' ? 'text-white' : 'text-blue-300'} ml-4 text-sm font-semibold md:text-lg`}
                 >
                   나의 크루
                 </Link>
                 <Link
                   href="/my-gathering"
-                  className={`${pathname === '/my-gathering' ? 'text-black' : 'text-white'} ml-4 font-bold`}
+                  className={`${pathname === '/my-gathering' ? 'text-white' : 'text-blue-300'} ml-4 text-sm font-semibold md:text-lg`}
                 >
-                  나의 모임
+                  나의 약속
                 </Link>
               </>
             ) : (
               <>
-                <Link href="/login" className="ml-4 font-bold text-white">
+                <Link href="/login" className="ml-4 text-sm font-semibold text-blue-300 md:text-lg">
                   나의 크루
                 </Link>
-                <Link href="/login" className="ml-4 font-bold text-white">
-                  나의 모임
+                <Link href="/login" className="ml-4 text-sm font-semibold text-blue-300 md:text-lg">
+                  나의 약속
                 </Link>
               </>
             )}
@@ -62,7 +67,7 @@ export default function HeaderPresenter({
           <div className="relative">
             <Menu trigger="hover" openDelay={100} closeDelay={400}>
               <Menu.Target>
-                <div className="mt-1">
+                <div className="mt-[6px]">
                   <Profile imageUrl={profileImageUrl} size="header" />
                 </div>
               </Menu.Target>
@@ -77,7 +82,7 @@ export default function HeaderPresenter({
             </Menu>
           </div>
         ) : (
-          <Link href="/login" className="font-bold text-white">
+          <Link href="/login" className="text-sm font-semibold text-white md:text-lg">
             로그인
           </Link>
         )}
