ee1e85e07723b95cc08fba49e72df82831626b42
Lee Youl <2yulrang@gmail.com>
2024-10-30 13:59:23 +0900

    🐛 Fix: it-time-overed를 is-today로 변경

--- src/components/gathering-detail-modal/presenter.tsx
+++ src/components/gathering-detail-modal/presenter.tsx
@@ -1,10 +1,10 @@
 import Image from 'next/image';
 import { Button, Modal, ScrollArea } from '@mantine/core';
 import { formatDate } from '@/src/utils/format-date';
-import isTimeOvered from '@/src/utils/is-time-overed';
 import { GatheringDetailType } from '@/src/types/gathering-data';
 import IcoClock from '@/public/assets/icons/ic-clock.svg';
 import IcoUser from '@/public/assets/icons/ic-user.svg';
+import isToday from '@/src/utils/is-today';
 
 export interface GatheringDetailModalProps {
   opened: boolean;
@@ -45,7 +45,7 @@ export default function GatheringDetailModalPresenter({
             fill
             style={{ width: '100%', height: '100%', objectFit: 'cover' }}
           />
-          {isTimeOvered(data.dateTime) && (
+          {isToday(data.dateTime) && (
             <strong className="absolute right-0 top-0 flex items-center gap-2 bg-blue-600 px-4 py-2 text-base font-medium text-white">
               <Image src={IcoClock} width={15} height={13} alt="아이콘" />
               <span>오늘 {new Date(data.dateTime).getHours()}시 마감</span>
