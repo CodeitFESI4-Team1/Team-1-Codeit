caa379062fe6fcae079ddb84c47ea4a3d7fb6ce5
User850413 <csyou1228@gmail.com>
2024-10-31 00:09:27 +0900

    :test_tube: test: Storybook에 무한스크롤 적용

--- src/components/common/crew-list/crew-card-list.stories.tsx
+++ src/components/common/crew-list/crew-card-list.stories.tsx
@@ -1,30 +1,56 @@
 import type { Meta, StoryObj } from '@storybook/react';
-import { crewData } from '@/src/mock/crew-dats';
-import CrewCardList from './crew-card-list';
+import { useInfiniteScroll } from '@/src/hooks/useInfiniteScroll';
+import { fetchCrewData } from '@/src/app/api/mock-api/crew';
+import { CrewCardInformResponse } from '@/src/types/crew-card';
+import ClientProvider from '../../client-provider';
+import CrewCardList, { CrewCardListProps } from './crew-card-list';
 
 const meta: Meta = {
   title: 'Components/CrewCardList/CrewCardList',
   component: CrewCardList,
   parameters: {
     layout: 'centered',
+    nextjs: {
+      appDirectory: true,
+    },
   },
   tags: ['autodocs'],
   args: {
-    CrewCardInforms: crewData,
+    // CrewCardInforms: crewData,
   },
+  decorators: [
+    (Story) => (
+      <ClientProvider>
+        <Story />
+      </ClientProvider>
+    ),
+  ],
 } satisfies Meta<typeof CrewCardList>;
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
+function RenderCrewCardList({ isWide = false }: Pick<CrewCardListProps, 'isWide'>) {
+  const { data, ref, isFetchingNextPage } = useInfiniteScroll<CrewCardInformResponse>({
+    queryKey: ['crew'],
+    queryFn: ({ pageParam = 0 }) => {
+      return fetchCrewData(pageParam, 3);
+    },
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.hasNextPage ? allPages.length + 1 : undefined,
+  });
+
+  return (
+    <CrewCardList data={data} ref={ref} isWide={isWide} isFetchingNextPage={isFetchingNextPage} />
+  );
+}
+
 export const Default: Story = {
-  args: {
-    isWide: false,
-  },
+  render: () => <RenderCrewCardList />,
+  args: {},
 };
 
-export const isWide: Story = {
-  args: {
-    isWide: true,
-  },
+export const IsWide: Story = {
+  render: () => <RenderCrewCardList isWide />,
+  args: {},
 };
