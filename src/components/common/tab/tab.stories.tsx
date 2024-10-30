77da5fd12b7b21c4de8406dad8b4065250664a6a
HaeJungg <gpwnd0120@gmail.com>
2024-10-30 14:21:16 +0900

    🧪 test: 스토리북 추가

---
+++ src/components/common/tab/tab.stories.tsx
@@ -0,0 +1,56 @@
+import React, { useState } from 'react';
+import { action } from '@storybook/addon-actions';
+import type { Meta, StoryFn } from '@storybook/react';
+import tabData from '@/src/data/tab.json';
+import Tabs, { TabsProps } from './index';
+
+const meta: Meta = {
+  title: 'Components/Tabs',
+  component: Tabs,
+  tags: ['autodocs'],
+  argTypes: {
+    tabs: {
+      description: '탭 목록 데이터 배열',
+      control: 'object',
+    },
+    activeTab: {
+      description: '현재 활성화된 탭 ID',
+      control: 'text',
+    },
+    onTabClick: {
+      description: '탭 클릭 시 호출되는 함수',
+    },
+  },
+};
+
+export default meta;
+
+const Template: StoryFn<TabsProps> = function TabsStory({ activeTab, tabs, ...args }: TabsProps) {
+  const [currentTab, setCurrentTab] = useState(activeTab || tabs[0].id);
+
+  return (
+    <Tabs
+      {...args}
+      tabs={tabs}
+      activeTab={currentTab}
+      onTabClick={(id) => {
+        setCurrentTab(id);
+        action('onTabClick')(id);
+      }}
+    />
+  );
+};
+
+// AppointmentTabs 스토리
+export const AppointmentTabs = Template.bind({});
+AppointmentTabs.args = {
+  tabs: tabData.appointmentTabs,
+  activeTab: tabData.appointmentTabs[0].id,
+};
+
+// CrewTabs 스토리
+export const CrewTabs = Template.bind({});
+CrewTabs.args = {
+  tabs: tabData.crewTabs,
+  activeTab: tabData.crewTabs[0].id,
+};
