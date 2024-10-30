d42ed12f10f182cd3c5593e86e66eb08fdecccf8
Lee Youl <2yulrang@gmail.com>
2024-10-30 16:22:06 +0900

    Fix: 린트오류 제거

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,3 @@
-import CustomButton from '@/components/input/CustomButton';
-
 export default function Home() {
-  return (
-    <div className="container">
-      CREW CREW
-      <CustomButton label="CustomButton" />
-    </div>
-  );
+  return <div className="container">CREW CREW</div>;
 }
