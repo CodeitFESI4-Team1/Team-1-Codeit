bfb9c983d28c6aef43d0a990e451ff3bc1b53cc0
min5x5 <minkyung5x5@gmail.com>
2024-10-30 14:43:09 +0900

    ✨ Feat: LoginForm 추가, 🧪 test: LoginForm storybook 추가

---
+++ src/app/login/_component/login-form.stories.tsx
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Meta, StoryFn } from '@storybook/react';
+import LoginForm from './login-form';
+
+const meta: Meta = {
+  title: 'Components/Auth/LoginForm',
+  component: LoginForm,
+};
+
+export default meta;
+
+const Template: StoryFn = function LoginFormStory() {
+  return <LoginForm />;
+};
+
+export const Default = Template.bind({});
+Default.args = {};
