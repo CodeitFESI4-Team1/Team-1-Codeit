14d7b1cdb3edd65439292cf4cf274ad677d638e9
HaeJungg <gpwnd0120@gmail.com>
2024-10-31 15:24:30 +0900

    ✨ feat: 확인 취소, 확인 모달 추가

---
+++ src/components/common/modal/confirm-modal.tsx
@@ -0,0 +1,56 @@
+import { ReactNode } from 'react';
+import { Modal } from '@mantine/core';
+import Button from '@/src/components/common/button/index';
+
+interface ConfirmModalProps {
+  children: ReactNode;
+  opened: boolean;
+  onClose: () => void;
+  onConfirm: () => void;
+}
+
+/**
+ * ConfirmModal
+ *
+ * @param {ReactNode} children - 모달 안에 들어갈 내용
+ * @param {boolean} opened - 모달이 열려있는지 여부
+ * @param {() => void} onClose - 모달을 닫기 위한 함수
+ * @param {() => void} onConfirm - 확인 버튼을 클릭했을 때 실행되는 함수
+ *
+ * import { useDisclosure } from '@mantine/hooks';사용
+ *
+ * <ConfirmModal opened={confirmOpened} onClose={closeConfirm} onConfirm={handleConfirm}>
+ *   {children}
+ * </ConfirmModal>
+ *
+ */
+
+export default function ConfirmModal({ children, opened, onClose, onConfirm }: ConfirmModalProps) {
+  const handleConfirm = () => {
+    onConfirm();
+    onClose();
+  };
+
+  return (
+    <Modal
+      opened={opened}
+      onClose={onClose}
+      centered
+      withCloseButton
+      size="xs"
+      overlayProps={{
+        opacity: 0.5,
+        blur: 2,
+      }}
+    >
+      <div className="space-y-8 text-center">
+        {children}
+        <div className="flex justify-end">
+          <Button className="btn-filled w-32" onClick={handleConfirm}>
+            확인
+          </Button>
+        </div>
+      </div>
+    </Modal>
+  );
+}
