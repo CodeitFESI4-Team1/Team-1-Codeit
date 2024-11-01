c4b4deea10751b9463656b11a7c1eba38043f6c6
HaeJungg <gpwnd0120@gmail.com>
2024-11-01 18:03:05 +0900

    💄 design: 크루카드 디자인 변경

--- src/components/common/crew-list/crew-card.tsx
+++ src/components/common/crew-list/crew-card.tsx
@@ -3,25 +3,9 @@
 import { useState } from 'react';
 import Image from 'next/image';
 import { useRouter } from 'next/navigation';
-import { useMediaQuery } from '@mantine/hooks';
+import ProgressBar from '@/src/components/common/progress-bar/index';
 import Check from '@/public/assets/icons/ic-check.svg';
-import Person from '@/public/assets/icons/ic-person.svg';
-import ProgressBar from '../progress-bar';
-import Profiles from './profiles';
-
-/**
- * CrewCard 컴포넌트
- * @param {number} id - 크루 id
- * @param {string} name - 크루 이름
- * @param {string} location - 크루 지역
- * @param {number} participantCount - 현재 참여 인원
- * @param {number} capacity - 수용 인원
- * @param {boolean} isConfirmed - 개설확정여부
- * @param {string} thumbnail - 메인 이미지
- * @param {boolean} isWide - wide된 상태에서 달라지는 ui 적용
- * @param {boolean} isAlone - 리스트에 속하지 않고 혼자 쓰이는 카드인지
- * @returns {JSX.Element}
- */
+import UserIco from '@/public/assets/icons/ic-user.svg';
 
 interface CrewCardProps {
   id: number;
@@ -31,9 +15,6 @@ interface CrewCardProps {
   capacity: number;
   isConfirmed: boolean;
   thumbnail: string;
-  isWide: boolean;
-  isAlone?: boolean;
-  isClickable?: boolean;
   gatheringCount: number;
 }
 
@@ -45,9 +26,6 @@ export default function CrewCard({
   capacity,
   isConfirmed,
   thumbnail,
-  isAlone = false,
-  isWide = !!isAlone,
-  isClickable = true,
   gatheringCount,
 }: CrewCardProps) {
   const [prefetched, setPrefetched] = useState(new Set());
@@ -55,83 +33,55 @@ export default function CrewCard({
   const router = useRouter();
 
   const handleCardClick = () => {
-    if (isClickable) {
-      router.push(CREWPAGE);
-    }
+    router.push(CREWPAGE);
   };
 
   const handleCardMouseUp = () => {
-    if (isClickable && !prefetched.has(CREWPAGE)) {
+    if (!prefetched.has(CREWPAGE)) {
       router.prefetch(CREWPAGE);
       setPrefetched(new Set(prefetched).add(CREWPAGE));
     }
   };
 
-  // NOTE: maxLength에 따라 ... 로 줄이는 함수
-  function truncatedText({ text, maxLength }: { text: string; maxLength: number }) {
-    const truncated = text.length > maxLength ? `${text.slice(0, maxLength)}...` : text;
-    return truncated;
-  }
-
-  const isTablet = useMediaQuery('(min-width: 745px) and (max-width: 1200px)');
-  const isDesktop = useMediaQuery('(min-width:1201px)');
-  let textLength;
-
-  if (isTablet) {
-    textLength = 12;
-  } else if (isDesktop) {
-    textLength = 20;
-  } else {
-    textLength = 11;
-  }
-
   return (
     <div
       role="presentation"
-      onClick={isClickable ? handleCardClick : undefined}
-      onMouseEnter={isClickable ? handleCardMouseUp : undefined}
-      className={`relative flex h-fit cursor-pointer flex-col overflow-hidden rounded-[14px] bg-white shadow-bg md:flex-row ${isAlone ? 'w-[369px] md:h-[270px] md:w-[770px] lg:w-[1108px]' : 'w-full md:h-[203px]'}`}
+      onClick={() => router.push(CREWPAGE)}
+      className="relative mx-auto flex h-[430px] w-full cursor-pointer flex-col overflow-hidden rounded-[14px] bg-white shadow-bg md:h-[203px] md:flex-row"
     >
-      <span
-        className={`relative w-full flex-shrink-0 md:w-1/2 ${!isWide ? 'lg:w-[203px]' : ''} ${isAlone ? 'h-[270px]' : 'h-[167px] md:h-full'}`}
-      >
+      {/* 썸네일 */}
+      <div className="relative h-[203px] w-full flex-shrink-0 md:w-[230px] lg:w-[200px]">
         <Image fill objectFit="cover" alt={name} src={thumbnail} />
-      </span>
-      <div className="flex w-full flex-col justify-normal gap-8 p-4 md:justify-between md:gap-0">
+      </div>
+
+      <div className="flex w-full flex-col justify-between p-6 sm:h-[238px] sm:px-4 sm:pt-4 md:h-[203px]">
         <div>
-          <div
-            className={`flex flex-col items-start gap-2 ${!isWide ? 'lg:flex-col lg:items-start' : 'md:flex-row md:items-center'}`}
-          >
-            <span className="typo-xl-semibold">
-              {truncatedText({ text: name, maxLength: textLength })}
+          <div className="flex flex-col gap-1">
+            <span className="max-w-[200px] overflow-hidden text-ellipsis whitespace-nowrap pr-4 text-lg font-semibold">
+              {name}
             </span>
-            <span className="typo-base-medium">| {location}</span>
+            <span className="text-base font-medium">| {location}</span>
           </div>
-          <span className="typo-sm-semibold text-blue-600">
+          <span className="text-sm font-semibold text-blue-600">
             {`현재 ${gatheringCount}개의 약속이 개설되어 있습니다.`}
           </span>
         </div>
-        <div
-          className={`flex w-full gap-8 border-t-[2px] border-t-[#E5E7EB] pt-[31px] ${!isWide ? 'lg:gap-4' : ''}`}
-        >
+        <div className="flex w-full gap-8 pt-[31px]">
           <div className="flex flex-grow flex-col items-start gap-2">
-            <span className="typo-sm-medium flex w-full items-center justify-between">
-              <span className="flex items-center">
-                <Image src={Person} alt="모임 인원" width={20} height={20} />
-                <span>
+            <div className="flex w-full items-center justify-between">
+              <div className="flex items-center space-x-2">
+                <Image src={UserIco} alt="user icon" width={20} height={20} />
+                <span className="text-base font-medium">
                   {participantCount}/{capacity}
                 </span>
-                <span className="ml-[22px] hidden md:block">
-                  <Profiles id={2} shows={2} />
-                </span>
-              </span>
-
+              </div>
               {isConfirmed && (
-                <span className="flex items-center gap-[2px] text-blue-600">
-                  <Image src={Check} alt="확인" width={24} height={24} /> <span> 개설 확정</span>
+                <span className="flex items-center gap-[1px] text-blue-600">
+                  <Image src={Check} alt="확인" width={24} height={24} />
+                  <span className="text-sm font-medium"> 개설 확정</span>
                 </span>
               )}
-            </span>
+            </div>
             <ProgressBar total={capacity} current={participantCount} />
           </div>
         </div>
