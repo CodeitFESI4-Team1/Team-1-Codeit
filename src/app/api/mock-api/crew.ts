6fa3ce229358fcdca62b81afc792e0bbe3a09c32
User850413 <csyou1228@gmail.com>
2024-11-01 15:02:59 +0900

    :sparkles: feat: mock api

--- src/app/api/mock-api/crew.ts
+++ src/app/api/mock-api/crew.ts
@@ -1,7 +1,9 @@
 import { CrewCardInformResponse } from '@/src/types/crew-card';
+import { ReviewInformResponse } from '@/src/types/review';
 import { crewData } from '@/src/mock/crew-data';
+import { CrewReviewData, MyReviewData } from '@/src/mock/review-data';
 
-// NOTE : 임시 API
+// NOTE: 크루 데이터 fetch
 export const fetchCrewData = (page: number, limit: number) => {
   const startIndex = page * limit;
   const endIndex = startIndex + limit;
@@ -17,12 +19,41 @@ export const fetchCrewData = (page: number, limit: number) => {
   });
 };
 
-// NOTE : useInfiniteScroll과 함께 쓰기
 // const { data, ref, isFetchingNextPage } = useInfiniteScroll<CrewCardInformResponse>({
 //   queryKey: ['crew'],
-//   queryFn: ({ pageParam = 0 }) => {
-//     return fetchCrewData(pageParam, 3);
-//   },
+//   queryFn: ({ pageParam = 0 }) => fetchCrewData(pageParam, 3);
 //   getNextPageParam: (lastPage, allPages) =>
 //     lastPage.hasNextPage ? allPages.length + 1 : undefined,
 // });
+
+// NOTE : 크루 리뷰 데이터 fetch
+export const fetchCrewReviewData = (page: number, limit: number) => {
+  const startIndex = page * limit;
+  const endIndex = startIndex + limit;
+  const data = CrewReviewData.data.slice(startIndex, endIndex);
+
+  return new Promise<ReviewInformResponse>((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data,
+        hasNextPage: endIndex < crewData.data.length,
+      });
+    });
+  });
+};
+
+// NOTE : 크루 리뷰 데이터 fetch
+export const fetchMyReviewData = (page: number, limit: number) => {
+  const startIndex = page * limit;
+  const endIndex = startIndex + limit;
+  const data = MyReviewData.data.slice(startIndex, endIndex);
+
+  return new Promise<ReviewInformResponse>((resolve) => {
+    setTimeout(() => {
+      resolve({
+        data,
+        hasNextPage: endIndex < crewData.data.length,
+      });
+    });
+  });
+};
