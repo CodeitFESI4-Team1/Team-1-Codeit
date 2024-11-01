705407b5a7bac4ebef305a49f79738680d16fdbc
HaeJungg <gpwnd0120@gmail.com>
2024-11-01 18:20:28 +0900

    🐛 fix: 스토리북 오류 수정

--- src/components/common/crew-list/detail-crew-card.stories.tsx
+++ src/components/common/crew-list/detail-crew-card.stories.tsx
@@ -1,3 +1,4 @@
+// CrewCard 스토리북 파일
 import type { Meta, StoryObj } from '@storybook/react';
 import CrewCard from './detail-crew-card';
 
@@ -16,6 +17,19 @@ const meta: Meta = {
     name: '같이 물장구칠사람',
     location: '충청',
     thumbnail: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
+    gatheringCount: 5, // 기본 값 추가
+    crewList: [
+      {
+        id: 1,
+        nickname: 'John',
+        imageUrl: 'https://i.pinimg.com/564x/e2/25/bb/e225bb492dc7a20a549f3c0abec28eb8.jpg',
+      },
+      {
+        id: 2,
+        nickname: 'Jane',
+        imageUrl: 'https://i.pinimg.com/564x/e2/25/bb/e225bb492dc7a20a549f3c0abec28eb8.jpg',
+      },
+    ], // 기본 프로필 리스트 추가
   },
 } satisfies Meta<typeof CrewCard>;
 
