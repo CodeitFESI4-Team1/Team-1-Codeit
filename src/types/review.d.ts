b4180f112be91bbcf34c8b206af9765075a740f6
User850413 <csyou1228@gmail.com>
2024-11-01 14:39:08 +0900

    :recycle: refactor: 리뷰 데이터 및 리뷰 타입 빼기

---
+++ src/types/review.d.ts
@@ -0,0 +1,24 @@
+// NOTE: 크루 전체 리뷰
+export interface CrewReview {
+  crewId: number;
+  id: number;
+  rate: number;
+  comment: string;
+  createdAt: string;
+  reviewer: ReviewerType;
+}
+
+export interface ReviewerType {
+  id: number;
+  nickname: string;
+  imageUrl: string;
+}
+
+// NOTE: 나의 리뷰
+export interface MyReview {
+  crewName: string;
+  gatheringName: string;
+  id: number; // 리뷰 id
+  rate: number;
+  comment: string;
+}
