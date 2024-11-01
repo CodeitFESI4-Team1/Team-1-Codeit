d108447173df7bc74bb09b965c8b1f635b4a0dd6
User850413 <csyou1228@gmail.com>
2024-11-01 17:25:23 +0900

    :test_tube: test: storybook 수정

---
+++ src/components/common/review-list/review-card-list.stories.tsx
@@ -0,0 +1,59 @@
+import type { Meta, StoryObj } from '@storybook/react';
+import { useInfiniteScroll } from '@/src/hooks/useInfiniteScroll';
+import { fetchCrewReviewData, fetchMyReviewData } from '@/src/app/api/mock-api/review';
+import { ReviewInformResponse } from '@/src/types/review';
+import ClientProvider from '../../client-provider';
+import ReviewCardList from './review-card-list';
+
+const meta: Meta<typeof ReviewCardList> = {
+  title: 'components/ReviewCardList',
+  component: ReviewCardList,
+  parameters: {
+    layout: 'centered',
+    nextjs: {
+      appDirectory: true,
+    },
+  },
+  tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <ClientProvider>
+        <Story />
+      </ClientProvider>
+    ),
+  ],
+} satisfies Meta<typeof ReviewCardList>;
+
+export default meta;
+type Story = StoryObj<typeof ReviewCardList>;
+
+function RenderCrewReviewCardList() {
+  const { data, ref, isFetchingNextPage } = useInfiniteScroll<ReviewInformResponse>({
+    queryKey: ['review'],
+    queryFn: ({ pageParam = 0 }) => fetchCrewReviewData(pageParam, 3),
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.hasNextPage ? allPages.length + 1 : undefined,
+  });
+  return <ReviewCardList data={data} ref={ref} isFetchingNextPage={isFetchingNextPage} />;
+}
+
+function RenderMyReviewCardList() {
+  const { data, ref, isFetchingNextPage } = useInfiniteScroll<ReviewInformResponse>({
+    queryKey: ['review'],
+    queryFn: ({ pageParam = 0 }) => fetchMyReviewData(pageParam, 3),
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.hasNextPage ? allPages.length + 1 : undefined,
+  });
+
+  return <ReviewCardList data={data} ref={ref} isFetchingNextPage={isFetchingNextPage} />;
+}
+
+export const CrewReviewCardList: Story = {
+  render: () => <RenderCrewReviewCardList />,
+  args: {},
+};
+
+export const MyReviewCardList: Story = {
+  render: () => <RenderMyReviewCardList />,
+  args: {},
+};
