d07ca3091de85d4a20c559e34702fd29ae7893b5
HaeJungg <gpwnd0120@gmail.com>
2024-11-05 13:47:21 +0900

    ✨ feat: 약속 디테일 모달 연결

--- src/components/common/gathering-card/container.tsx
+++ src/components/common/gathering-card/container.tsx
@@ -1,6 +1,9 @@
 'use client';
 
 import { useState } from 'react';
+import { useDisclosure } from '@mantine/hooks';
+import { GatheringDetailType } from '@/src/types/gathering-data';
+import GatheringDetailModalContainer from '../../gathering-detail-modal/container';
 import GatheringCardPresenter from './presenter';
 
 interface GatheringCardContainerProps {
@@ -16,6 +19,41 @@ interface GatheringCardContainerProps {
   className?: string;
 }
 
+const dummyGatheringData: GatheringDetailType = {
+  id: 1,
+  title: '모임이름',
+  dateTime: '2024-12-31T10:00:00',
+  location: '경기 과천시 중앙동 관악산',
+  currentCount: 2,
+  totalCount: 10,
+  imageUrl:
+    'https://images.unsplash.com/photo-1601758260892-a62c486ace97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+  isLiked: false,
+  introduce: '이 모임은 예시 모임입니다. 함께 즐거운 시간을 보낼 수 있는 모임이에요!',
+  isCaptain: true,
+  isParticipant: true,
+  participants: [
+    {
+      id: 1,
+      profileImageUrl: 'https://i.pinimg.com/564x/e2/25/bb/e225bb492dc7a20a549f3c0abec28eb8.jpg',
+      nickname: '가나다',
+      email: 'hong@example.com',
+    },
+    {
+      id: 2,
+      profileImageUrl: 'https://i.pinimg.com/564x/9d/b8/86/9db886bb5475cc35a7f450831f4125bc.jpg',
+      nickname: '라마바',
+      email: 'kim@example.com',
+    },
+    {
+      id: 3,
+      profileImageUrl: '',
+      nickname: '가나다',
+      email: 'lee@example.com',
+    },
+  ],
+};
+
 export default function GatheringCard({
   id,
   title,
@@ -27,6 +65,8 @@ export default function GatheringCard({
   isLiked: initialIsLiked,
   className,
 }: GatheringCardContainerProps) {
+  const [opened, { open, close }] = useDisclosure(false);
+
   // 날짜 비교
   const gatheringDate = new Date(dateTime);
   const today = new Date();
@@ -47,23 +87,32 @@ export default function GatheringCard({
     setIsLiked((prev) => !prev);
   };
 
+  const openModal = () => {
+    // TODO: 모임 상세보기 API 연결
+    open();
+  };
+
   return (
-    <GatheringCardPresenter
-      id={id}
-      isPast={isPast}
-      deadlineMessage={deadlineMessage}
-      isWithin24Hours={isWithin24Hours}
-      title={title}
-      dateTime={dateTime}
-      location={location}
-      currentCount={currentCount}
-      totalCount={totalCount}
-      imageUrl={imageUrl}
-      isLiked={isLiked}
-      onLikeToggle={handleLikeToggle}
-      // eslint-disable-next-line no-alert
-      onClick={() => alert('카드클릭!')}
-      className={className}
-    />
+    <>
+      <GatheringCardPresenter
+        id={id}
+        isPast={isPast}
+        deadlineMessage={deadlineMessage}
+        isWithin24Hours={isWithin24Hours}
+        title={title}
+        dateTime={dateTime}
+        location={location}
+        currentCount={currentCount}
+        totalCount={totalCount}
+        imageUrl={imageUrl}
+        isLiked={isLiked}
+        onLikeToggle={handleLikeToggle}
+        onClick={openModal}
+        className={className}
+      />
+      {opened && (
+        <GatheringDetailModalContainer opened={opened} close={close} data={dummyGatheringData} />
+      )}
+    </>
   );
 }
