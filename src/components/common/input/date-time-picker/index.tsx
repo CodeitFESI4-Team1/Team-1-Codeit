be077cc176fa438608be7dcb9ce01c13bd2b392e
Lee Youl <2yulrang@gmail.com>
2024-11-05 15:10:01 +0900

    💄 Design: 디자인 맞추고 필요없는 파일 삭제

--- src/components/common/input/date-time-picker/index.tsx
+++ src/components/common/input/date-time-picker/index.tsx
@@ -1,6 +1,6 @@
 import { useState } from 'react';
 import { Button } from '@mantine/core';
-import { Calendar } from '@mantine/dates';
+import { Calendar, DatesProvider } from '@mantine/dates';
 import dayjs from 'dayjs';
 import 'dayjs/locale/ko';
 import DropDown from '@/src/components/common/input/drop-down';
@@ -35,37 +35,41 @@ export default function DateTimePicker({ fullDate, onChange }: DateTimePickerPro
   return (
     <div className="flex flex-col items-center gap-5">
       <div className="rounded-xl border border-gray-200 px-9 pb-4 pt-2.5">
-        <Calendar
-          size="xl"
-          getDayProps={(date) => ({
-            selected: selected && dayjs(date).isSame(selected, 'date'),
-            onClick: () => handleSelect(date),
-          })}
-          firstDayOfWeek={0}
-          classNames={{
-            day: 'w-full aspect-square text-gray-800 data-[selected=true]:text-white flex',
-            monthCell: 'w-[calc(14.285vw-5.714px)] aspect-square md:w-[85px]',
-            monthsListCell:
-              'w-[calc(33.333vw-13.333px)] h-[10vw] md:w-[200px] md:h-[80px] text-gray-800',
-            monthsListControl: 'w-full h-full',
-            yearsListCell:
-              'w-[calc(33.333vw-13.333px)] h-[10vw] md:w-[200px] md:h-[80px] text-gray-800',
-          }}
-          weekendDays={[]}
-          previousIcon={<IconArrow direction="left" color="#1F2937" />}
-          nextIcon={<IconArrow direction="right" color="#1F2937" />}
-          styles={{
-            weekday: { fontSize: '1rem', fontWeight: '500', color: '#1F2937' },
-          }}
-          renderDay={(date) => {
-            const day = date.getDate();
-            return (
-              <div data-testid="day" className="text-base font-medium lg:text-lg">
-                {day}
-              </div>
-            );
-          }}
-        />
+        <DatesProvider
+        settings={{ locale: 'ko', firstDayOfWeek: 0, weekendDays: [0], timezone: 'UTC' }}
+        >
+          <Calendar
+            size="xl"
+            getDayProps={(date) => ({
+              selected: selected && dayjs(date).isSame(selected, 'date'),
+              onClick: () => handleSelect(date),
+            })}
+            firstDayOfWeek={0}
+            classNames={{
+              day: 'w-full aspect-square text-gray-800 data-[selected=true]:text-white flex',
+              monthCell: 'w-[calc(14.285vw-5.714px)] aspect-square md:w-[85px]',
+              monthsListCell:
+                'w-[calc(33.333vw-13.333px)] h-[10vw] md:w-[200px] md:h-[80px] text-gray-800',
+              monthsListControl: 'w-full h-full',
+              yearsListCell:
+                'w-[calc(33.333vw-13.333px)] h-[10vw] md:w-[200px] md:h-[80px] text-gray-800',
+            }}
+            weekendDays={[0]}
+            previousIcon={<IconArrow direction="left" color="#1F2937" />}
+            nextIcon={<IconArrow direction="right" color="#1F2937" />}
+            styles={{
+              weekday: { fontSize: '1rem', fontWeight: '500', color: '#1F2937' },
+            }}
+            renderDay={(date) => {
+              const day = date.getDate();
+              return (
+                <div data-testid="day" className="text-base font-medium lg:text-lg">
+                  {day}
+                </div>
+              );
+            }}
+          />
+        </DatesProvider>
       </div>
       <div>
         <label htmlFor="time" className="text-base font-semibold text-gray-800">
