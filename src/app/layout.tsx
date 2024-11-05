4a0d17cb92e9461d5028e58bedda7cce999906dc
HaeJungg <gpwnd0120@gmail.com>
2024-11-05 16:30:26 +0900

    ✨ feat: notification 추가

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,9 +1,10 @@
 import type { Metadata } from 'next';
+import { pretendard } from '@/src/fonts/pretendard/pretendard';
 import { ColorSchemeScript, MantineProvider } from '@mantine/core';
 import '@mantine/core/styles.css';
+import { Notifications } from '@mantine/notifications';
 import ClientProvider from '@/src/components/client-provider';
 import Header from '@/src/components/common/header/container';
-import { pretendard } from '@/src/fonts/pretendard/pretendard';
 import '@/src/styles/globals.css';
 import { theme } from '@/src/styles/theme';
 
@@ -26,10 +27,12 @@ export default function RootLayout({
       <body className="flex min-h-screen flex-col bg-gray-50">
         <ClientProvider>
           <MantineProvider theme={theme}>
-            <Header />
-            <div className="mx-auto flex w-full flex-1 flex-col lg:max-w-[1200px]">
-              <main className="flex-1 md:shadow-bg">{children}</main>
-            </div>
+            <Notifications>
+              <Header />
+              <div className="mx-auto flex w-full flex-1 flex-col lg:max-w-[1200px]">
+                <main className="flex-1 md:shadow-bg">{children}</main>
+              </div>
+            </Notifications>
           </MantineProvider>
         </ClientProvider>
       </body>
