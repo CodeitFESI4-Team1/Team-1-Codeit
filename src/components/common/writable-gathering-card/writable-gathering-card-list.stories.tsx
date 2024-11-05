876e3b614a227501e80694b12c641154d3c10800
User850413 <csyou1228@gmail.com>
2024-11-05 16:36:10 +0900

    :sparkles: feat: 데이터 삽입 #57

--- src/components/common/writable-gathering-card/writable-gathering-card-list.stories.tsx
+++ src/components/common/writable-gathering-card/writable-gathering-card-list.stories.tsx
@@ -1,15 +1,47 @@
 import type { Meta, StoryObj } from '@storybook/react';
+import { useInfiniteScroll } from '@/src/hooks/useInfiniteScroll';
+import { fetchWritableGatheringData } from '@/src/app/api/mock-api/writable-gathering';
+import { WritableGatheringCardInformResponse } from '@/src/types/writable-gathering-card';
+import ClientProvider from '../../client-provider';
 import WritableGatheringCardList from './writable-gathering-card-list';
 
 const meta: Meta = {
   title: 'Components/WritableGatheringCardList',
   component: WritableGatheringCardList,
+  parameters: {
+    layout: 'centered',
+    nextjs: {
+      appDirectory: true,
+    },
+  },
   tags: ['autodocs'],
+  decorators: [
+    (Story) => (
+      <ClientProvider>
+        <Story />
+      </ClientProvider>
+    ),
+  ],
 } satisfies Meta<typeof WritableGatheringCardList>;
+
+function RenderWritableGatheringCardList() {
+  const { data, ref, isFetchingNextPage } = useInfiniteScroll<WritableGatheringCardInformResponse>({
+    queryKey: ['crew'],
+    queryFn: ({ pageParam = 0 }) => {
+      return fetchWritableGatheringData(pageParam, 3);
+    },
+    getNextPageParam: (lastPage, allPages) =>
+      lastPage.hasNextPage ? allPages.length + 1 : undefined,
+  });
+  return (
+    <WritableGatheringCardList data={data} isFetchingNextPage={isFetchingNextPage} ref={ref} />
+  );
+}
 
 export default meta;
 type Story = StoryObj<typeof meta>;
 
 export const Default: Story = {
+  render: () => <RenderWritableGatheringCardList />,
   args: {},
 };
