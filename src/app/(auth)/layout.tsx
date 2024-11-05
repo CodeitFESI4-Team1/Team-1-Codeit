a4b85031b80e057a0b9894e95128402b66896ba7
min5x5 <minkyung5x5@gmail.com>
2024-11-05 14:24:20 +0900

    💄 Style: 디자인에 맞게 레이아웃 UI 수정

--- src/app/(auth)/layout.tsx
+++ src/app/(auth)/layout.tsx
@@ -19,10 +19,19 @@ export default function AuthLayout({
     <div className="flex h-screen flex-col bg-gray-50">
       <Header />
       <div className="flex flex-1">
-        <div className="hidden h-full w-2/3 items-center md:flex">
+        <div className="hidden h-full items-center md:flex md:w-1/2 lg:w-2/3">
           <Image src={Auth} alt="auth" />
         </div>
-        <div className="flex h-full w-full bg-white md:w-1/3 md:items-center">{children}</div>
+        <div className="flex h-full w-full flex-col items-center bg-white p-6 md:w-1/2 md:justify-center md:p-8 lg:w-1/3 lg:p-20">
+          <div className="text-xl font-semibold md:text-2xl lg:text-3xl">Welcome,</div>
+          <div className="text-center text-xl font-semibold md:text-2xl lg:text-3xl">
+            크루에 오신 것을 환영합니다 🙌
+          </div>
+          <div className="mt-4 text-center text-sm font-semibold lg:text-base">
+            함께할 사람이없나요? 지금 크루에 참여해보세요
+          </div>
+          <div className="mt-6 w-full md:mt-12 md:w-2/3">{children}</div>
+        </div>
       </div>
     </div>
   );
