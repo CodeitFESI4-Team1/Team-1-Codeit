01257f38af2f46af5a2e9333b636bf4dbf6f3249
HaeJungg <gpwnd0120@gmail.com>
2024-11-05 14:09:49 +0900

    ✨ feat: 크루 리뷰부분 조립

--- src/app/detail/[id]/page.tsx
+++ src/app/detail/[id]/page.tsx
@@ -3,6 +3,7 @@ import { getGatheringList } from '@/src/_apis/detail/get-gatheringList';
 import Button from '@/src/components/common/button';
 import DetailCrewCard from '@/src/components/common/crew-list/detail-crew-card';
 import GatheringCardCarousel from '@/src/components/gathering-list/gathering-card-carousel';
+import CrewReviewSection from './_components/review-section';
 
 export default async function CrewDetailPage() {
   const crewDetail = await getCrewDetail();
@@ -12,21 +13,28 @@ export default async function CrewDetailPage() {
     <div className="container mx-auto my-0 min-h-screen max-w-pc bg-gray-50 py-11 shadow-bg">
       <div className="space-y-10">
         <section className="lg:mx-12">
-          <DetailCrewCard {...crewDetail} />
+          <article>
+            <DetailCrewCard {...crewDetail} />
+          </article>
         </section>
         <section className="space-y-6 md:mx-8 lg:mx-12">
-          <div className="flex items-center justify-between">
-            <h2 className="text-2xl font-semibold">크루 약속 잡기</h2>
-            <Button type="button" className="btn-filled px-4">
-              약속 만들기
-            </Button>
-          </div>
-          <div className="flex">
-            <GatheringCardCarousel gatheringData={gatheringList} />
-          </div>
+          <article className="space-y-6">
+            <div className="flex items-center justify-between">
+              <h2 className="text-2xl font-semibold">크루 약속 잡기</h2>
+              <Button type="button" className="btn-filled px-4">
+                약속 만들기
+              </Button>
+            </div>
+            <div className="flex">
+              <GatheringCardCarousel gatheringData={gatheringList} />
+            </div>
+          </article>
         </section>
         <section className="md:mx-8 lg:mx-12">
-          <h2 className="text-2xl font-semibold">크루 리뷰</h2>
+          <article className="space-y-6">
+            <h2 className="text-2xl font-semibold">크루 리뷰</h2>
+            <CrewReviewSection />
+          </article>
         </section>
       </div>
     </div>
