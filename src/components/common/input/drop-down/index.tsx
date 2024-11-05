5922225d34a7900650a85f6e2e58f9cf8bbf67f4
leeyoul <2yulrang@gmail.com>
2024-11-05 11:05:45 +0900

    Feat/cr 58/create crew page (#41)
    * ✨ Feat: calendar-filter 컴포넌트 작성
    
    * 🧪 Test: 스토리북 추가
    
    * 💚 CI: 빌드 오류 수정
    
    * 📦 Chore: 페이지 폴더 변경
    
    * 📦 Chore: 페이지 폴더 변경, 패키지 추가
    
    * 💄 Design : 반응형 스타일 적용
    
    * 🚚 Chore: 폴더 위치 변경
    
    * Update and rename deploy.yml to trigger.yml
    
    * Update dispatch.yml
    
    * Update and rename trigger.yml to deploy.yml
    
    * Update dispatch.yml
    
    * Create dispatch_develop.yml
    
    * Create deploy_develop.yml
    
    * Update dispatch_develop.yml
    
    * Update deploy_develop.yml
    
    * Update deploy.yml
    
    * Update deploy_develop.yml
    
    * Update dispatch.yml
    
    * Update dispatch_develop.yml
    
    * ✨ Feat: 텍스트인풋, 드롭다운, 파일인풋 추가
    
    * ♻️ Refactor: 드롭다운 수정 반영
    
    * ✨ Feat: 페이지 초안 작성
    
    * 💄 Style: 프리티어 적용
    
    * 💄 Design: 디자인 적용, 파일 이동
    
    * 💄 Design: 디자인 적용, 파일 이동
    
    * 🚨 Fix: 린트 오류 수정
    
    * 🚨 Fix: 빌드 오류 수정
    
    * 🧪 Test: 스토리북 추가가
    
    * 🐛 Fix: 빌드 오류 수정
    
    * 🚨 Fix: 빌드 오류 수정
    
    * 🚚 Chore: 파일 이동
    
    * ✨ Feat: 유효성 검사 추가
    
    * 🔧 Chore: 스토리북 빌드 오류 때문에 설정 수정
    
    * 🔧 Chore: 설정 제거
    
    * 🐛 Fix: dropdown 다른 곳에 영향 안가도록 수정
    
    * 🐛 Fix: onBlur 빼내기
    
    * 🚚 Chore: 파일 이동, CSR로 전환
    
    * 🎨 Style : 린트프리티어 적용
    
    * 🐛 Fix: 오류 수정
    
    * 🐛 Fix: 버튼 교체
    
    * 🐛 Fix: TextInput 교체
    
    * 🔧 Chore: config 변경
    
    * 🐛 Fix: 스토리북 빌드오류 수정
    
    * 🐛 Fix: 스토리북 빌드오류 수정
    
    * 🚚 Chore: 파일 이동
    
    * 🚨 Fix: 린트 오류 수정
    
    * 🐛 Fix: 같은 파일 업로드시 업로드 되지 않는 문제 해결
    
    * 🐛 Fix: 한글입력시 21자에서 되돌아가는 문제 수정
    
    * 🐛 Fix: 블러시 함수 충돌문제 해결

--- src/components/common/input/drop-down/index.tsx
+++ src/components/common/input/drop-down/index.tsx
@@ -1,5 +1,6 @@
-import { useState } from 'react';
-import { ComboboxData, ComboboxItem, Select, SelectProps } from '@mantine/core';
+import { LegacyRef, useEffect, useState } from 'react';
+import { Controller, useForm } from 'react-hook-form';
+import { ComboboxData, Select, SelectProps } from '@mantine/core';
 import IconArrow from '@/public/assets/icons/ic-arrow';
 
 /**
@@ -14,56 +15,100 @@ import IconArrow from '@/public/assets/icons/ic-arrow';
 export interface DropDownProps {
   variant: 'default' | 'sort';
   data: ComboboxData;
+  name: string;
   placeholder: string;
   value: string | null;
-  onChange: (newValue: string | null, option: ComboboxItem) => void;
-  className: string;
+  onChange: (newValue: string | null) => void;
+  className?: string;
+  inWhere?: string;
+  ref?: LegacyRef<HTMLInputElement>;
 }
 
 export default function DropDown({
+  inWhere = 'default',
+  name,
   variant,
   data,
   value,
   onChange,
   placeholder,
   className,
-  ...args
+  ref,
+  ...rest
 }: DropDownProps) {
+  const {
+    control,
+    trigger,
+    formState: { errors },
+  } = useForm();
   const [isFocused, setIsFocused] = useState(false);
 
   const renderSelectOption: SelectProps['renderOption'] = ({ option }) => (
     <div data-testid="dropDownOption">{option.label}</div>
   );
 
+  const getColor = () => {
+    if (isFocused) return '#ffffff';
+    if (inWhere === 'form') return '#1F2937';
+    return '#D1D5DB';
+  };
+
+  useEffect(() => {
+    if (value === null) {
+      onChange(null);
+    }
+  }, [value]);
+
   return (
-    <Select
-      mt="md"
-      data={data}
-      value={value}
-      onFocus={() => setIsFocused(true)}
-      onBlur={() => setIsFocused(false)}
-      onChange={onChange}
-      withCheckIcon={false}
-      leftSection=""
-      rightSection={
-        variant !== 'sort' && (
-          <IconArrow direction="down" color={`${isFocused === true ? '#ffffff' : '#D1D5DB'}`} />
-        )
-      }
-      placeholder={placeholder}
-      classNames={{
-        wrapper: `${className}`,
-        input: `${variant === 'sort' && (isFocused ? 'sort-bg-on pl-10' : 'sort-bg pl-10')} font-pretendard h-10 md:h-11 focus:bg-black focus:placeholder:text-white focus:text-white text-base font-medium rounded-xl border-0 py-2.5 px-3 placeholder-gray-800 `,
-        section: `end-1 ${variant === 'sort' && 'data-[position=right]:hidden start-1'}`,
-        dropdown: 'rounded-xl shadow-xl p-0 border-0 mt-2',
-        option: `py-1 m-1 px-2 mr-0 rounded-xl font-pretendard text-gray-800 text-base font-medium hover:bg-blue-100 ${variant === 'sort' && 'justify-center'}`,
-      }}
-      styles={{
-        root: { '--mantine-spacing-md': 0 },
-      }}
-      comboboxProps={{ position: 'bottom', middlewares: { flip: false, shift: false }, offset: 0 }}
-      {...args}
-      renderOption={renderSelectOption}
+    <Controller
+      name={name}
+      control={control}
+      rules={{ required: '필수 입력사항입니다.', validate: (inputValue) => inputValue !== '' }}
+      render={({ field }) => (
+        <Select
+          {...field}
+          error={errors[name] && '필수 입력사항입니다.'}
+          mt="md"
+          data={data}
+          value={value}
+          name={name}
+          dropdownOpened={isFocused}
+          onFocus={() => setIsFocused(true)}
+          onBlur={() => {
+            if (inWhere === 'form') trigger(field.name); // react-hook-form의 onBlur 호출
+            if (typeof field.onBlur === 'function') {
+              setIsFocused(false); // 기존 Select onBlur 호출
+            }
+          }}
+          onChange={(newValue) => {
+            field.onChange(newValue); // react-hook-form의 onChange 호출
+            onChange(newValue); // 기존 Select onChange 호출
+            setIsFocused(false);
+          }}
+          color={getColor()}
+          leftSection=""
+          rightSection={variant !== 'sort' && <IconArrow direction="down" color={getColor()} />}
+          placeholder={placeholder}
+          classNames={{
+            root: `${className}`,
+            input: `${variant === 'sort' && (isFocused ? 'sort-bg-on pl-10' : 'sort-bg pl-10')} ${inWhere === 'form' ? 'bg-gray-100 placeholder-gray-400' : 'bg-white placeholder-gray-800'} font-pretendard h-10 md:h-11 focus:bg-black focus:placeholder:text-white focus:text-white text-base font-medium rounded-xl border-0 py-2.5 px-3`,
+            section: `end-1 ${variant === 'sort' && 'data-[position=right]:hidden start-1'}`,
+            dropdown: 'rounded-xl shadow-xl p-0 border-0 mt-2',
+            option: `py-1 m-1 px-2 mr-0 rounded-xl font-pretendard text-gray-800 text-base font-medium hover:bg-blue-100 ${variant === 'sort' && 'justify-center'}`,
+          }}
+          styles={{
+            root: { '--mantine-spacing-md': 0 },
+          }}
+          comboboxProps={{
+            position: 'bottom',
+            middlewares: { flip: false, shift: false },
+            offset: 0,
+          }}
+          renderOption={renderSelectOption}
+          ref={ref as LegacyRef<HTMLInputElement>}
+          {...rest}
+        />
+      )}
     />
   );
 }
