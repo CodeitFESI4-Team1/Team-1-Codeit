b18146bd0d858678c8353dace4d85ff82eb06be5
User850413 <csyou1228@gmail.com>
2024-11-05 16:36:10 +0900

    :sparkles: feat: 데이터 삽입 #57

--- src/components/common/writable-gathering-card/writable-gathering-card.tsx
+++ src/components/common/writable-gathering-card/writable-gathering-card.tsx
@@ -1,5 +1,32 @@
-interface WritableGatheringCardProps {}
+import { ParticipantType } from '@/src/types/writable-gathering-card';
 
-export default function WritableGatheringCard() {
-  return <div>Card</div>;
+interface WritableGatheringCardProps {
+  id: number;
+  gatheringName: string;
+  dateTime: string;
+  currentCount: number;
+  totalCount: number;
+  imageUrl: string;
+  participants: ParticipantType[];
+}
+
+export default function WritableGatheringCard({
+  id,
+  currentCount,
+  dateTime,
+  gatheringName,
+  imageUrl,
+  participants,
+  totalCount,
+}: WritableGatheringCardProps) {
+  return (
+    <div>
+      <span>{id}</span>
+      <span>{currentCount}</span>
+      <span>{dateTime}</span>
+      <span>{gatheringName}</span>
+      <span>{imageUrl}</span>
+      <span>{totalCount}</span>
+    </div>
+  );
 }
