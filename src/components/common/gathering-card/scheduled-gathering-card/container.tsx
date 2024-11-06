eb84994733b4d8682ee913cfff451b53bacbe091
min5x5 <minkyung5x5@gmail.com>
2024-11-06 19:44:20 +0900

    🐛 fix: api dummydata로 임시 수정

--- src/components/common/gathering-card/scheduled-gathering-card/container.tsx
+++ src/components/common/gathering-card/scheduled-gathering-card/container.tsx
@@ -1,8 +1,5 @@
 import { useCallback, useState } from 'react';
-import { useQuery } from '@tanstack/react-query';
-import { useGetGatheringQuery } from '@/src/_queries/gathering/gathering-queries';
 import GatheringDetailModalContainer from '@/src/app/(crew)/crew/_components/gathering-detail-modal/container';
-import { GatheringDetailType } from '@/src/types/gathering-data';
 import ScheduledGatheringCardPresenter from './presenter';
 
 interface ScheduledGatheringCardContainerProps {
@@ -24,7 +21,45 @@ export default function ScheduledGatheringCardContainer({
   data,
 }: ScheduledGatheringCardContainerProps) {
   const [isOpened, setIsOpened] = useState(false);
-  const { data: modalData } = useQuery<GatheringDetailType>(useGetGatheringQuery());
+  // TODO: modalData 연결
+  // const { data: modalData } = useQuery<GatheringDetailType>(useGetGatheringQuery());
+  const dummyModalData = {
+    id: 1,
+    title: '신나는 운동...즐거운..코딩..',
+    introduce: '공지사항입니다. 다들 이번 약속 잊지 않으셨죠? 꼭 참여 부탁드립니다~',
+    dateTime: '2024-10-29T00:32:12.306Z',
+    location: '서울시 강남구 역삼동 오피스타워 3층',
+    currentCount: 3,
+    totalCount: 10,
+    imageUrl:
+      'https://www.dabur.com/Blogs/Doshas/Importance%20and%20Benefits%20of%20Yoga%201020x450.jpg',
+    isLiked: false,
+    isGatherCaptain: false,
+    isParticipant: false,
+    participants: [
+      {
+        id: 1,
+        profileImageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUMrcQB5OJ-ETzPc6wHnjxjC-36__MGw3JcA&s',
+        nickname: '럽윈즈올',
+        email: 'youl@email.com',
+      },
+      {
+        id: 2,
+        profileImageUrl:
+          'https://imgcdn.stablediffusionweb.com/2024/5/13/c0541236-e690-4dff-a27e-30a0355e5ea0.jpg',
+        nickname: '모닝러너',
+        email: 'youl@email.com',
+      },
+      {
+        id: 3,
+        profileImageUrl:
+          'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQUMrcQB5OJ-ETzPc6wHnjxjC-36__MGw3JcA&s',
+        nickname: '동글동글이',
+        email: 'youl@email.com',
+      },
+    ],
+  };
 
   const handleCardClick = useCallback(() => {
     setIsOpened(true);
@@ -42,13 +77,13 @@ export default function ScheduledGatheringCardContainer({
         onClick={handleCardClick}
         onLikeToggle={handleLikeToggle}
       />
-      {modalData && (
+      {dummyModalData && (
         <GatheringDetailModalContainer
           opened={isOpened}
           close={() => {
             setIsOpened(false);
           }}
-          data={modalData}
+          data={dummyModalData}
         />
       )}
     </div>
