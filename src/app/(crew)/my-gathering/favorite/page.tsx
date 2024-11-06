57f9da50d44086f102e7f902428d6e6a19a267ed
min5x5 <minkyung5x5@gmail.com>
2024-11-06 10:41:10 +0900

    💄 Style: gatheringlist max-w 삭제

--- src/app/(crew)/my-gathering/favorite/page.tsx
+++ src/app/(crew)/my-gathering/favorite/page.tsx
@@ -4,7 +4,7 @@ import { gatheringData } from '@/src/mock/gathering-data';
 export default function FavoritePage() {
   return (
     <div className="md:mt-[45px]">
-      <GatheringList gatheringData={gatheringData}></GatheringList>
+      <GatheringList gatheringData={gatheringData} />
     </div>
   );
 }
