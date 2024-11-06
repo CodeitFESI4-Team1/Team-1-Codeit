76df1211808b2a7b0276ee89e27ebdeaa2ba50ed
min5x5 <minkyung5x5@gmail.com>
2024-11-06 10:43:34 +0900

    💄 Style: 전체 레이아웃에 flex-1 추가

--- src/app/(crew)/layout.tsx
+++ src/app/(crew)/layout.tsx
@@ -11,7 +11,7 @@ export default function RootLayout({
     <>
       <Header />
       <div className="flex min-h-screen flex-col items-center bg-gray-50">
-        <main className="container flex max-w-pc flex-col md:shadow-bg">{children}</main>
+        <main className="container flex max-w-pc flex-1 flex-col md:shadow-bg">{children}</main>
       </div>
     </>
   );
