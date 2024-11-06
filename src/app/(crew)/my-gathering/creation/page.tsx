5ecbd768098ad241a767cdbd4a6d48f9664eeb2c
min5x5 <minkyung5x5@gmail.com>
2024-11-06 16:15:03 +0900

    ✨ Feat: 참여한 모임 페이지 추가

--- src/app/(crew)/my-gathering/creation/page.tsx
+++ src/app/(crew)/my-gathering/creation/page.tsx
@@ -12,7 +12,7 @@ export default function CreationPage() {
   const creationGatheringList: GatheringCardProps[] = [
     {
       id: 1,
-      crewTitle: '풀 엔 그레이스 스튜디오',
+      crewTitle: 'Power Pole',
       crewMainLocation: '서울시',
       crewSubLocation: '강남구 역삼동',
       title: '가나다라마가나다라마가나다라마가',
@@ -42,7 +42,20 @@ export default function CreationPage() {
       crewMainLocation: '서울시',
       crewSubLocation: '강남구 역삼동',
       title: '가나다라마가나다라마가나다라마가',
-      dateTime: '2024-10-31T00:30',
+      dateTime: '2024-11-11T00:30',
+      currentCount: 8,
+      totalCount: 12,
+      imageUrl:
+        'https://images.unsplash.com/photo-1601758260892-a62c486ace97?q=80&w=1470&auto=format&fit=crop&ixlib=rb-4.0.3&ixid=M3wxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHx8fA%3D%3D',
+      isLiked: true,
+    },
+    {
+      id: 4,
+      crewTitle: '풀 엔 그레이스 스튜디오',
+      crewMainLocation: '서울시',
+      crewSubLocation: '강남구 역삼동',
+      title: '가나다라마가나다라마가나다라마가',
+      dateTime: '2024-11-21T00:30',
       currentCount: 8,
       totalCount: 12,
       imageUrl:
@@ -52,8 +65,10 @@ export default function CreationPage() {
   ];
 
   return (
-    <div>
-      <PopOverCalendar value={selectedDate} onChange={(d) => setSelectedDate(d)} />
+    <div className="">
+      <div className="pl-4 md:pt-2">
+        <PopOverCalendar value={selectedDate} onChange={(d) => setSelectedDate(d)} />
+      </div>
       <GatheringListWithDate gatheringList={creationGatheringList} />
     </div>
   );
