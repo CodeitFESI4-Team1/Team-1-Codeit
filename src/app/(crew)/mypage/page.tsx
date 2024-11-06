0a907256e88b958091b9ab81a74a52617707b80b
User850413 <csyou1228@gmail.com>
2024-11-07 00:25:13 +0900

    :lipstick: design: UI 수정 #67

--- src/app/(crew)/mypage/page.tsx
+++ src/app/(crew)/mypage/page.tsx
@@ -74,7 +74,7 @@ export default function MyPage() {
     }
   };
   return (
-    <div className="container mx-auto my-0 min-h-screen max-w-pc bg-gray-50 px-3 py-11 shadow-bg md:px-8 lg:px-11">
+    <div className="container mx-auto my-0 min-h-screen max-w-pc bg-gray-50 px-3 py-11 md:px-8 lg:px-11">
       <div className="lg:gap-4.5 flex flex-col gap-3 md:gap-4">
         <ProfileCardContainer data={mockData} />
       </div>
