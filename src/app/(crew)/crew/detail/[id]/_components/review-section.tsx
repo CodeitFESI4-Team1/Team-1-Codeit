a0ebb42f3f2854f73a1eba86afbca133675a1978
HaeJungg <gpwnd0120@gmail.com>
2024-11-06 11:32:40 +0900

    ✨ feat: 크루 리뷰 조립

--- src/app/(crew)/crew/detail/[id]/_components/review-section.tsx
+++ src/app/(crew)/crew/detail/[id]/_components/review-section.tsx
@@ -1,25 +1,38 @@
-import RatingDisplay, { ReviewRateInfo } from './rating-display';
+'use client';
+
+import { useEffect, useState } from 'react';
+import { ReviewListData, getReviewList } from '@/src/_apis/detail/get-review-list';
+import CrewReviewList from './crew-review-list';
+import RatingDisplay from './rating-display';
 
 export default function CrewReviewSection() {
-  const exampleReviewRateInfo: ReviewRateInfo = {
-    totalRate: 15,
-    averageRate: 4.5,
-    ratingsData: [
-      { score: 5, count: 9 },
-      { score: 4, count: 4 },
-      { score: 3, count: 1 },
-      { score: 2, count: 1 },
-      { score: 1, count: 0 },
-    ],
-  };
+  const [reviewData, setReviewData] = useState<ReviewListData | null>(null);
+  const [currentPage, setCurrentPage] = useState(1);
+  const limit = 5;
+
+  useEffect(() => {
+    async function fetchReviewData() {
+      const data = await getReviewList(currentPage, limit);
+      setReviewData(data);
+    }
+    fetchReviewData();
+  }, [currentPage]);
+
+  const handlePageChange = (page: number) => setCurrentPage(page);
+
+  if (!reviewData) return <div>Loading...</div>;
 
   return (
     <div className="space-y-6 rounded-lg bg-white">
       <div className="mx-4 flex justify-center py-11">
-        <RatingDisplay reviewRateInfo={exampleReviewRateInfo} />
+        <RatingDisplay reviewRateInfo={reviewData.info} />
       </div>
-      {/* 리뷰 리스트 페이지네이션 */}
-      <div> 리뷰 리스트 페이지네이션 </div>
+      <CrewReviewList
+        reviews={reviewData.data}
+        totalPages={reviewData.totalPages}
+        currentPage={currentPage}
+        onPageChange={handlePageChange}
+      />
     </div>
   );
 }
