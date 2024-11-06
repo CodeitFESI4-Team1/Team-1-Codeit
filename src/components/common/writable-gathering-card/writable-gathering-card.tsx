9147cdab333e7b4c9dde9e8f12fbdf27e34c894f
Lee Youl <2yulrang@gmail.com>
2024-11-06 15:37:09 +0900

    🚨 Fix: 절대경로로 수정

--- src/components/common/writable-gathering-card/writable-gathering-card.tsx
+++ src/components/common/writable-gathering-card/writable-gathering-card.tsx
@@ -1,8 +1,8 @@
 import Image from 'next/image';
+import Profiles from '@/src/components/common/crew-list/profiles';
+import Button from '@/src/components/common/input/button';
 import { ParticipantType } from '@/src/types/writable-gathering-card';
 import person from '@/public/assets/icons/person.svg';
-import Button from '../button';
-import Profiles from '../crew-list/profiles';
 
 interface WritableGatheringCardProps {
   id: number;
