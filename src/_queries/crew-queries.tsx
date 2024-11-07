cb904c3382ef29599ea12808499c539c47ddc23c
leeyoul <2yulrang@gmail.com>
2024-11-07 16:34:14 +0900

    Hotfix: env 명칭변경 (#84)
    * Hotfix: env 명칭변경
    
    * ♻️ Refactor: fetchApi 함수 사용
    
    * 💄 Desgin: 스타일 수정
    
    * 🐛 Fix: 타입 개선, 에러 처리

--- src/_queries/crew-queries.tsx
+++ src/_queries/crew-queries.tsx
@@ -1,5 +1,5 @@
-import { getCrewData } from '@/src/app/api/mock-api/crew';
 import { CrewCardInform } from '@/src/types/crew-card';
+import { getCrewList } from '../_apis/crew/get-crew-list';
 
 // Crew data 변환 로직을 간소화한 helper 함수
 const mapCrewData = (crew: CrewCardInform) => ({
@@ -31,7 +31,7 @@ export function useGetCrewQuery() {
 
   return {
     queryKey: ['crew'],
-    queryFn: ({ pageParam = 0 }: QueryParams) => getCrewData(pageParam, 3),
+    queryFn: ({ pageParam = 0 }: QueryParams) => getCrewList(pageParam, 3),
     getNextPageParam: (lastPage: Page, allPages: Page[]) =>
       lastPage.hasNextPage ? allPages.length + 1 : undefined,
     select: (data: CrewCardInform[]) => data.map(mapCrewData),
