46ebdcf2dc6256fcca2d4959f57f9331a2055882
min5x5 <minkyung5x5@gmail.com>
2024-11-07 10:48:41 +0900

    🐛 fix: cors url 추가

--- next.config.mjs
+++ next.config.mjs
@@ -8,5 +8,25 @@ const nextConfig = {
       },
     ],
   },
+  async headers() {
+    return [
+      {
+        source: '/:path*', // TODO: 모든 경로에 적용, 추후 수정
+        headers: [
+          { key: 'Access-Control-Allow-Credentials', value: 'true' },
+          {
+            key: 'Access-Control-Allow-Origin',
+            value: 'https://json-server-vercel-crewcrew.vercel.app',
+          },
+          { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
+          {
+            key: 'Access-Control-Allow-Headers',
+            value:
+              'X-CSRF-Token, X-Requested-With, Accept, Accept-Version, Content-Length, Content-MD5, Content-Type, Date, X-Api-Version',
+          },
+        ],
+      },
+    ];
+  },
 };
 export default nextConfig;
