23473ec1cac2cb69830588b95e67a3ebe196dc7e
Lee Youl <2yulrang@gmail.com>
2024-11-07 16:30:23 +0900

    🐛 Fix: 타입 개선, 에러 처리

--- src/_apis/crew/get-crew-list.ts
+++ src/_apis/crew/get-crew-list.ts
@@ -2,16 +2,24 @@ import { fetchApi } from '@/src/utils/api';
 import { CrewCardInform, CrewCardInformResponse } from '@/src/types/crew-card';
 
 export async function getCrewList(page: number, limit: number): Promise<CrewCardInformResponse> {
-  const response = await fetchApi<CrewCardInformResponse>(
-    `/crews?_page=${page + 1}&_limit=${limit}`,
-    {
-      method: 'GET',
-      headers: {
-        'Content-Type': 'application/json',
+  try {
+    const response = await fetchApi<CrewCardInformResponse>(
+      `/crews?_page=${page + 1}&_limit=${limit}`,
+      {
+        method: 'GET',
+        headers: {
+          'Content-Type': 'application/json',
+        },
       },
-    },
-  );
-  const data = response as unknown as CrewCardInform[];
-  const hasNextPage = data.length === limit;
-  return { data, hasNextPage };
+    );
+    if (!Array.isArray(response)) {
+      throw new Error('서버 응답이 올바른 형식이 아닙니다.');
+    }
+    const data = response as CrewCardInform[];
+    const hasNextPage = data.length === limit;
+
+    return { data, hasNextPage };
+  } catch (error) {
+    throw new Error('크루 리스트를 불러오는데 실패했습니다.');
+  }
 }
