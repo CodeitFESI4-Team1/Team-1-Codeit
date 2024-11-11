1ec5e8313de23f9b31f768d00bffe78f42e0105a
HaeJungg <gpwnd0120@gmail.com>
2024-11-11 13:50:09 +0900

    🐛 fix: 백앤드 데이터라 따라서 크루부분 타입, 값 변경

--- src/mock/crew-data.ts
+++ src/mock/crew-data.ts
@@ -1,24 +1,42 @@
-import { CrewCardInform } from '../types/crew-card';
+interface CrewMember {
+  id: number;
+  nickname: string;
+  profileImageUrl: string;
+}
 
-export const crewData: { data: CrewCardInform[] } = {
+interface CrewDataItem {
+  id: number;
+  mainCategory: string;
+  subCategory: string;
+  title: string;
+  location: string;
+  subLocation: string;
+  participantCount: number;
+  totalCount: number;
+  imageUrl: string;
+  isConfirmed: boolean;
+  totalGatheringCount: number;
+  crewMember: CrewMember[];
+}
+
+interface CrewData {
+  data: CrewDataItem[];
+}
+
+export const crewData: CrewData = {
   data: [
     {
-      crewId: 1,
-      type: '유산소',
-      subType: '수영',
-      name: '엄청긴크루이름엄청긴크루이름엄청긴크루이름엄청긴크루이름',
+      id: 1,
+      mainCategory: '유산소',
+      subCategory: '수영',
+      title: '엄청긴크루이름엄청긴크루이름엄청긴크루이름엄청긴크루이름',
       location: '경기도',
-      detailedLocation: '어디구 어디로',
+      subLocation: '어디구 어디로',
       participantCount: 5,
-      capacity: 24,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 1,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
+      totalCount: 24,
+      imageUrl: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
       isConfirmed: true,
-      gatheringCount: 3,
+      totalGatheringCount: 3,
       crewMember: [
         {
           id: 1,
@@ -53,22 +71,17 @@ export const crewData: { data: CrewCardInform[] } = {
       ],
     },
     {
-      crewId: 2,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
+      id: 2,
+      mainCategory: '유산소',
+      subCategory: '달리기',
+      title: '같이 달릴사람 구함',
       location: '광주',
-      detailedLocation: '어디구 어디로',
+      subLocation: '어디구 어디로',
       participantCount: 10,
-      capacity: 20,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 1,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
+      totalCount: 20,
+      imageUrl: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
       isConfirmed: true,
-      gatheringCount: 3,
+      totalGatheringCount: 3,
       crewMember: [
         {
           id: 1,
@@ -133,22 +146,17 @@ export const crewData: { data: CrewCardInform[] } = {
       ],
     },
     {
-      crewId: 3,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
+      id: 3,
+      mainCategory: '유산소',
+      subCategory: '달리기',
+      title: '같이 달릴사람 구함',
       location: '부산',
-      detailedLocation: '어디구 어디로',
+      subLocation: '어디구 어디로',
       participantCount: 1,
-      capacity: 5,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 2,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
+      totalCount: 5,
+      imageUrl: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
       isConfirmed: false,
-      gatheringCount: 3,
+      totalGatheringCount: 3,
       crewMember: [
         {
           id: 1,
@@ -159,22 +167,17 @@ export const crewData: { data: CrewCardInform[] } = {
       ],
     },
     {
-      crewId: 4,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
+      id: 4,
+      mainCategory: '유산소',
+      subCategory: '달리기',
+      title: '같이 달릴사람 구함',
       location: '부산',
-      detailedLocation: '어디구 어디로',
+      subLocation: '어디구 어디로',
       participantCount: 1,
-      capacity: 20,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 2,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
-      gatheringCount: 3,
+      totalCount: 20,
+      imageUrl: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
       isConfirmed: false,
+      totalGatheringCount: 3,
       crewMember: [
         {
           id: 1,
@@ -185,22 +188,17 @@ export const crewData: { data: CrewCardInform[] } = {
       ],
     },
     {
-      crewId: 5,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
+      id: 5,
+      mainCategory: '유산소',
+      subCategory: '달리기',
+      title: '같이 달릴사람 구함',
       location: '부산',
-      detailedLocation: '어디구 어디로',
+      subLocation: '어디구 어디로',
       participantCount: 1,
-      capacity: 20,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 2,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
+      totalCount: 20,
+      imageUrl: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
       isConfirmed: false,
-      gatheringCount: 5,
+      totalGatheringCount: 5,
       crewMember: [
         {
           id: 1,
@@ -211,22 +209,17 @@ export const crewData: { data: CrewCardInform[] } = {
       ],
     },
     {
-      crewId: 6,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
+      id: 6,
+      mainCategory: '유산소',
+      subCategory: '달리기',
+      title: '같이 달릴사람 구함',
       location: '광주',
-      detailedLocation: '어디구 어디로',
+      subLocation: '어디구 어디로',
       participantCount: 10,
-      capacity: 20,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 1,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
+      totalCount: 20,
+      imageUrl: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
       isConfirmed: true,
-      gatheringCount: 5,
+      totalGatheringCount: 5,
       crewMember: [
         {
           id: 1,
@@ -291,22 +284,17 @@ export const crewData: { data: CrewCardInform[] } = {
       ],
     },
     {
-      crewId: 7,
-      type: '유산소',
-      subType: '수영',
-      name: '엄청긴크루이름엄청긴크루이름엄청긴크루이름엄청긴크루이름',
+      id: 7,
+      mainCategory: '유산소',
+      subCategory: '수영',
+      title: '엄청긴크루이름엄청긴크루이름엄청긴크루이름엄청긴크루이름',
       location: '경기도',
-      detailedLocation: '어디구 어디로',
+      subLocation: '어디구 어디로',
       participantCount: 5,
-      capacity: 24,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 1,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
+      totalCount: 24,
+      imageUrl: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
       isConfirmed: true,
-      gatheringCount: 5,
+      totalGatheringCount: 5,
       crewMember: [
         {
           id: 1,
@@ -341,22 +329,17 @@ export const crewData: { data: CrewCardInform[] } = {
       ],
     },
     {
-      crewId: 8,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
+      id: 8,
+      mainCategory: '유산소',
+      subCategory: '달리기',
+      title: '같이 달릴사람 구함',
       location: '광주',
-      detailedLocation: '어디구 어디로',
+      subLocation: '어디구 어디로',
       participantCount: 10,
-      capacity: 20,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 1,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
+      totalCount: 20,
+      imageUrl: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
       isConfirmed: true,
-      gatheringCount: 5,
+      totalGatheringCount: 5,
       crewMember: [
         {
           id: 1,
@@ -421,22 +404,17 @@ export const crewData: { data: CrewCardInform[] } = {
       ],
     },
     {
-      crewId: 9,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
+      id: 9,
+      mainCategory: '유산소',
+      subCategory: '달리기',
+      title: '같이 달릴사람 구함',
       location: '부산',
-      detailedLocation: '어디구 어디로',
+      subLocation: '어디구 어디로',
       participantCount: 3,
-      capacity: 5,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 2,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
+      totalCount: 5,
+      imageUrl: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
       isConfirmed: false,
-      gatheringCount: 5,
+      totalGatheringCount: 5,
       crewMember: [
         {
           id: 1,
@@ -459,22 +437,17 @@ export const crewData: { data: CrewCardInform[] } = {
       ],
     },
     {
-      crewId: 10,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
+      id: 10,
+      mainCategory: '유산소',
+      subCategory: '달리기',
+      title: '같이 달릴사람 구함',
       location: '부산',
-      detailedLocation: '어디구 어디로',
+      subLocation: '어디구 어디로',
       participantCount: 1,
-      capacity: 20,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 2,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
+      totalCount: 20,
+      imageUrl: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
       isConfirmed: false,
-      gatheringCount: 5,
+      totalGatheringCount: 5,
       crewMember: [
         {
           id: 1,
@@ -485,240 +458,17 @@ export const crewData: { data: CrewCardInform[] } = {
       ],
     },
     {
-      crewId: 11,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
+      id: 11,
+      mainCategory: '유산소',
+      subCategory: '달리기',
+      title: '같이 달릴사람 구함',
       location: '부산',
-      detailedLocation: '어디구 어디로',
+      subLocation: '어디구 어디로',
       participantCount: 1,
-      capacity: 20,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 2,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
+      totalCount: 20,
+      imageUrl: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg',
       isConfirmed: false,
-      gatheringCount: 5,
-      crewMember: [
-        {
-          id: 1,
-          nickname: 'User1',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/e2/25/bb/e225bb492dc7a20a549f3c0abec28eb8.jpg',
-        },
-      ],
-    },
-    {
-      crewId: 12,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
-      location: '광주',
-      detailedLocation: '어디구 어디로',
-      participantCount: 10,
-      capacity: 20,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 1,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
-      isConfirmed: true,
-      gatheringCount: 5,
-      crewMember: [
-        {
-          id: 1,
-          nickname: 'User1',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/e2/25/bb/e225bb492dc7a20a549f3c0abec28eb8.jpg',
-        },
-        {
-          id: 2,
-          nickname: 'User2',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/9d/b8/86/9db886bb5475cc35a7f450831f4125bc.jpg',
-        },
-        {
-          id: 3,
-          nickname: 'User3',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/41/56/d8/4156d8253b6d76e5455d28b44bd1a1e0.jpg',
-        },
-        {
-          id: 4,
-          nickname: 'User3',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/41/56/d8/4156d8253b6d76e5455d28b44bd1a1e0.jpg',
-        },
-        {
-          id: 5,
-          nickname: 'User1',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/e2/25/bb/e225bb492dc7a20a549f3c0abec28eb8.jpg',
-        },
-        {
-          id: 6,
-          nickname: 'User3',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/41/56/d8/4156d8253b6d76e5455d28b44bd1a1e0.jpg',
-        },
-        {
-          id: 7,
-          nickname: 'User1',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/e2/25/bb/e225bb492dc7a20a549f3c0abec28eb8.jpg',
-        },
-        {
-          id: 8,
-          nickname: 'User2',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/9d/b8/86/9db886bb5475cc35a7f450831f4125bc.jpg',
-        },
-        {
-          id: 9,
-          nickname: 'User3',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/41/56/d8/4156d8253b6d76e5455d28b44bd1a1e0.jpg',
-        },
-        {
-          id: 10,
-          nickname: 'User3',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/41/56/d8/4156d8253b6d76e5455d28b44bd1a1e0.jpg',
-        },
-      ],
-    },
-    {
-      crewId: 13,
-      type: '유산소',
-      subType: '수영',
-      name: '엄청긴크루이름엄청긴크루이름엄청긴크루이름엄청긴크루이름',
-      location: '경기도',
-      detailedLocation: '어디구 어디로',
-      participantCount: 2,
-      capacity: 24,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 1,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
-      isConfirmed: true,
-      gatheringCount: 6,
-      crewMember: [
-        {
-          id: 1,
-          nickname: 'User1',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/e2/25/bb/e225bb492dc7a20a549f3c0abec28eb8.jpg',
-        },
-        {
-          id: 2,
-          nickname: 'User2',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/9d/b8/86/9db886bb5475cc35a7f450831f4125bc.jpg',
-        },
-      ],
-    },
-    {
-      crewId: 14,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
-      location: '광주',
-      detailedLocation: '어디구 어디로',
-      participantCount: 10,
-      capacity: 20,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 1,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
-      isConfirmed: true,
-      gatheringCount: 0,
-      crewMember: [
-        {
-          id: 1,
-          nickname: 'User1',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/e2/25/bb/e225bb492dc7a20a549f3c0abec28eb8.jpg',
-        },
-        {
-          id: 2,
-          nickname: 'User2',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/9d/b8/86/9db886bb5475cc35a7f450831f4125bc.jpg',
-        },
-        {
-          id: 3,
-          nickname: 'User3',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/41/56/d8/4156d8253b6d76e5455d28b44bd1a1e0.jpg',
-        },
-        {
-          id: 4,
-          nickname: 'User3',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/41/56/d8/4156d8253b6d76e5455d28b44bd1a1e0.jpg',
-        },
-        {
-          id: 5,
-          nickname: 'User1',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/e2/25/bb/e225bb492dc7a20a549f3c0abec28eb8.jpg',
-        },
-        {
-          id: 6,
-          nickname: 'User3',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/41/56/d8/4156d8253b6d76e5455d28b44bd1a1e0.jpg',
-        },
-        {
-          id: 7,
-          nickname: 'User1',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/e2/25/bb/e225bb492dc7a20a549f3c0abec28eb8.jpg',
-        },
-        {
-          id: 8,
-          nickname: 'User2',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/9d/b8/86/9db886bb5475cc35a7f450831f4125bc.jpg',
-        },
-        {
-          id: 9,
-          nickname: 'User3',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/41/56/d8/4156d8253b6d76e5455d28b44bd1a1e0.jpg',
-        },
-        {
-          id: 10,
-          nickname: 'User3',
-          profileImageUrl:
-            'https://i.pinimg.com/564x/41/56/d8/4156d8253b6d76e5455d28b44bd1a1e0.jpg',
-        },
-      ],
-    },
-    {
-      crewId: 15,
-      type: '유산소',
-      subType: '달리기',
-      name: '같이 달릴사람 구함',
-      location: '부산',
-      detailedLocation: '어디구 어디로',
-      participantCount: 1,
-      capacity: 5,
-      images: [
-        { imagePath: 'https://i.pinimg.com/564x/f8/8d/c5/f88dc5b857caf6c303ae5ef9dd12e7fb.jpg' },
-      ],
-      createdBy: 2,
-      createdDate: new Date('2024-11-04'),
-      updatedDate: new Date('2024-11-05'),
-      isConfirmed: false,
-      gatheringCount: 5,
+      totalGatheringCount: 5,
       crewMember: [
         {
           id: 1,
