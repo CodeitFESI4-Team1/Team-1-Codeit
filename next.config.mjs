826a942066b30188e41bd701f2770433de2461e3
HaeJungg <gpwnd0120@gmail.com>
2024-11-11 13:30:37 +0900

    🎨 design: prettier changed

--- next.config.mjs
+++ next.config.mjs
@@ -18,7 +18,6 @@ const nextConfig = {
             key: 'Access-Control-Allow-Origin',
             value:
               process.env.NEXT_PUBLIC_API_URL || 'https://foggy-sideways-saltasaurus.glitch.me/',
-
           },
           { key: 'Access-Control-Allow-Methods', value: 'GET,OPTIONS,PATCH,DELETE,POST,PUT' },
           {
