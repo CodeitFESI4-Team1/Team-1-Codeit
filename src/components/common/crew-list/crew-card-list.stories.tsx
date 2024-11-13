a8816c8a9a02e3e74a1f236e13e8ee7de4a5057b
leeyoul <2yulrang@gmail.com>
2024-11-13 18:06:10 +0900

    Feat/98/crew api 연결 : 제가올린것중 이것먼저 리뷰 부탁드립니다~! (#113)
    * Squashed commit of the following:
    
    commit 51cfe2f249f98957a1a52b9a7c8a42c4613ca04d
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Sat Nov 9 11:40:05 2024 +0900
    
        🐛 Fix: MockData 날짜 형식 수정
    
    commit 7b68352e6861f91e0ef4c016833b26120f52f3b1
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Sat Nov 9 11:38:06 2024 +0900
    
        🐛 Fix: 이전날짜 선택 방지, isToday 날짜 비교 수정
    
    commit f1019879753e24f5532ba5da8127cc178e8e1a86
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Sat Nov 9 11:19:20 2024 +0900
    
        🐛 Fix: 폼 동작 개선
    
    commit c40805820abb07d5a513709aa986cc66b352098b
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Sat Nov 9 10:51:40 2024 +0900
    
        🐛 Fix: isToday 함수 개선, 데이터 한국시간 기준으로 변경
    
    commit 0683f5adaa0e33cc206d6db70b768cb1e175f3a2
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Fri Nov 8 21:49:45 2024 +0900
    
        🐛 Fix: 버튼 링크로 교체
    
    commit 43cdfa77813379825abf93898934fdcc65750a50
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Fri Nov 8 21:43:11 2024 +0900
    
        🚨 Fix: 린트 오류 제거
    
    commit 9657201427ff9a58f21adebc31a3eef50cebf07c
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Fri Nov 8 21:34:56 2024 +0900
    
        🐛 Fix: 스크롤바 두개로 보이는 문제 수정
    
    commit 63a8e1cdd0a9a75d853e7af5db287026296d2578
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Fri Nov 8 21:27:25 2024 +0900
    
        🐛 Fix: 유효성 검사 날짜 선택 오류 수정
    
    commit 80f855478a42b84e16a9001d01201d9b79af5e3b
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Fri Nov 8 21:24:46 2024 +0900
    
        🐛 Fix: 에러상태인데 새로 값 채울 때 에러 해제하기, 리팩토링
    
    commit af9fb914e36d87a9e4c39a69e3ea7a3d9a1e8206
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Fri Nov 8 20:46:00 2024 +0900
    
        🐛 Fix: 크루 생성시 디테일 페이지로 이동, 리팩토링
    
    commit f1fe204fefda189c9aac82889a4b19556a7a221f
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Fri Nov 8 20:31:17 2024 +0900
    
        🐛 Fix: 텍스트 수정(약속 잡기 > 약속 만들기)
    
    commit 5cc6746c1149904696ab1281fe93e2dd091f78ab
    Author: Lee Youl <2yulrang@gmail.com>
    Date:   Fri Nov 8 20:29:52 2024 +0900
    
        🐛 Fix: 드롭다운 값 선택시 포커스 아웃 되도록 수정
    
    * [WIP] Feat: GetCrewList API 연결
    
    * ✨ Feat: 백엔드 api 연결
    
    * 🐛 Fix: 처음 로딩시 카테고리 초기화
    
    * 🐛 Fix: 카테고리 동작 개선
    
    * 🐛 Fix: 지역 필터링 동작 개선
    
    * ✨ Feat: 지역 전체 필터 추가
    
    * 🚨 Fix: 타입 개선, 빌드 오류 수정
    
    * 🚧 Chore: workflow 수정
    
    * ♻️ Refactor 코드 개선
    
    * 🐛 Fix: 코드 개선
    
    * ✨ Feat: 검색 버튼 추가
    
    * 🐛 Fix: 동작 개선
    
    * 🚧 Chore: 경로 변경
    
    * 🐛 Fix: 콘솔에러 추가
    
    * ⚡️ Fix: 검색 엔터방식으로 변경
    
    * 🐛 Fix: 동작개선, 에러메시지 보이기
    
    * 💄 Design: 스타일 수정
    
    * 🐛 Fix: 에러처리 추가
    
    * 🚨 Fix: 빌드 오류 수정
    
    * 🐛 Fix: 타입 수정
    
    * 🐛 Fix: 디폴트이미지 삭제, 데브툴 조건부렌더링
    
    * 🐛 Fix: 렌더링 방식 변경
    
    * 🚨 Fix: 빌드 오류 수정
    
    * 🐛 Fix: env 추가
    
    * 🚚 Chore: 파일 이동, 파일명 변경
    
    * 🚚 Chore: 파일명 변경
    
    * ♿️ Fix: 접근성 개선

--- src/components/common/crew-list/crew-card-list.stories.tsx
+++ src/components/common/crew-list/crew-card-list.stories.tsx
@@ -1,7 +1,7 @@
 import { useEffect, useState } from 'react';
 import type { Meta, StoryObj } from '@storybook/react';
 import { InfiniteData } from '@tanstack/react-query';
-import { useGetCrewListQuery } from '@/src/_queries/crew-queries';
+import { useGetCrewListQuery } from '@/src/_queries/crew/crew-list-queries';
 import { useInfiniteScroll } from '@/src/hooks/use-infinite-scroll';
 import ClientProvider from '@/src/components/client-provider';
 import { MainCrewListResponse } from '@/src/types/crew-card';
@@ -29,17 +29,8 @@ const meta: Meta = {
 export default meta;
 type Story = StoryObj<typeof meta>;
 
-function RenderCrewCardList({
-  initialData,
-}: {
-  initialData: InfiniteData<MainCrewListResponse | undefined>;
-}) {
-  const [data, setData] = useState<InfiniteData<MainCrewListResponse | undefined>>(initialData);
-  const {
-    data: CrewCardListData,
-    ref,
-    isFetchingNextPage,
-  } = useInfiniteScroll(
+function RenderCrewCardList() {
+  const { data, ref, isFetchingNextPage } = useInfiniteScroll(
     useGetCrewListQuery({
       keyword: '',
       mainLocation: '',
@@ -49,16 +40,11 @@ function RenderCrewCardList({
     }),
   );
 
-  useEffect(() => {
-    if (CrewCardListData) {
-      setData(CrewCardListData);
-    }
-  }, [CrewCardListData]);
-
+  if (!data) return null;
   return <CrewCardList data={data} ref={ref} isFetchingNextPage={isFetchingNextPage} />;
 }
 
 export const Default: Story = {
-  render: () => <RenderCrewCardList initialData={{ pages: [], pageParams: [] }} />,
+  render: () => <RenderCrewCardList />,
   args: {},
 };
