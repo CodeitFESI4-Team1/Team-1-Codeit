79105a1501d9a806a4eea9755f5895e140ea668d
HaeJungg <gpwnd0120@gmail.com>
2024-11-14 19:09:03 +0900

    ✨ feat: 데이터 연결

--- src/app/(crew)/my-page/_components/profile-card/container.tsx
+++ src/app/(crew)/my-page/_components/profile-card/container.tsx
@@ -1,14 +1,35 @@
 'use client';
 
-import { UserType } from '@/src/types/user';
+import { useEffect, useState } from 'react';
+import { useRouter } from 'next/navigation';
+import { useAuthStore } from '@/src/store/use-auth-store';
 import ProfileCardPresenter from './presenter';
 
-export interface ProfileCardTypes {
-  data: UserType;
-}
+export default function ProfileCard() {
+  const router = useRouter();
+  const { user, isAuth } = useAuthStore();
+  const [isLoading, setIsLoading] = useState(true);
 
-export default function ProfileCard({ data }: ProfileCardTypes) {
-  // TODO : API 연결
-  const handleEdit = () => {};
-  return <ProfileCardPresenter data={data} onEdit={handleEdit} />;
+  useEffect(() => {
+    // 로딩 상태를 위해 약간의 지연 추가
+    const timer = setTimeout(() => {
+      if (!isAuth) {
+        router.push('/login');
+      } else {
+        setIsLoading(false);
+      }
+    }, 500);
+
+    return () => clearTimeout(timer);
+  }, [isAuth, router]);
+
+  if (isLoading) return <div>로딩 중...</div>;
+
+  if (!user) return <div>유저 정보를 불러오지 못했습니다.</div>;
+
+  const handleEdit = () => {
+    // 프로필 수정 기능 추가
+  };
+
+  return <ProfileCardPresenter data={user} onEdit={handleEdit} />;
 }
--- src/app/(crew)/my-page/page.tsx
+++ src/app/(crew)/my-page/page.tsx
@@ -1,18 +1,11 @@
 import ProfileCardContainer from '@/src/app/(crew)/my-page/_components/profile-card/container';
 import ReviewSection from '@/src/app/(crew)/my-page/_components/review-section';
 
-const mockData = {
-  id: 1,
-  profileImageUrl: '',
-  nickname: '율율',
-  email: 'youlyoul@email.com',
-};
-
 export default function MyPage() {
   return (
     <div className="container mx-auto my-0 min-h-screen max-w-pc bg-gray-50 px-3 py-11 md:px-8 lg:px-11">
       <div className="lg:gap-4.5 flex flex-col gap-3 md:gap-4">
-        <ProfileCardContainer data={mockData} />
+        <ProfileCardContainer />
       </div>
       <ReviewSection />
       <div />
