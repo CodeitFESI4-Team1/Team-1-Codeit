a518393c4e47060e13a78949641effc19b315d6c
HaeJungg <gpwnd0120@gmail.com>
2024-11-15 17:43:08 +0900

    ✨ feat: 크루 api 연결

--- src/_apis/crew/crew-detail-apis.ts
+++ src/_apis/crew/crew-detail-apis.ts
@@ -1,6 +1,7 @@
 import { fetchApi } from '@/src/utils/api';
 import { CrewDetail } from '@/src/types/crew-card';
 
+// 크루 디테일 보기
 export async function getCrewDetail(id: number): Promise<CrewDetail> {
   const url = `/api/crews/${id}`;
 
@@ -11,4 +12,40 @@ export async function getCrewDetail(id: number): Promise<CrewDetail> {
     },
   });
   return response.data;
+}
+
+// 크루 참여
+export async function joinCrew(crewId: number): Promise<void> {
+  const url = `/api/crews/${crewId}/join`;
+
+  await fetchApi<void>(url, {
+    method: 'POST',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
+// 사용자 크루 탈퇴
+export async function leaveCrew(crewId: number): Promise<void> {
+  const url = `/api/crews/${crewId}/leave`;
+
+  await fetchApi<void>(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
+}
+
+// 주최자 크루 취소
+export async function cancelCrew(crewId: number): Promise<void> {
+  const url = `/api/crews/${crewId}`;
+
+  await fetchApi<void>(url, {
+    method: 'DELETE',
+    headers: {
+      'Content-Type': 'application/json',
+    },
+  });
 }
