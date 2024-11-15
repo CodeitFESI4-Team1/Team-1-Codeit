b49e833dc37eac983e5d7221ae8406fa133610d3
HaeJungg <gpwnd0120@gmail.com>
2024-11-15 16:28:00 +0900

    ✨ feat: 새로운 프로필 타입 추가

--- src/components/common/profile/index.tsx
+++ src/components/common/profile/index.tsx
@@ -1,7 +1,9 @@
 'use client';
 
 import Image from 'next/image';
+import { cn } from '@/src/utils/cn';
 import defaultImage from '@/public/assets/icons/default-profile.svg';
+import captainCheck from '@/public/assets/icons/ic-captain-check.svg';
 import editImage from '@/public/assets/icons/profile-edit.svg';
 
 /**
@@ -11,6 +13,7 @@ import editImage from '@/public/assets/icons/profile-edit.svg';
  * @param {'small' | 'medium' | 'large'} [props.size='large'] - 프로필의 크기를 결정 기본값은 'large'
  * @param {string} [props.imageUrl] - 이미지 URL을 설정 없으면 기본 이미지
  * @param {boolean} [props.editable=false] - 프로필 편집시 사용
+ * @param {boolean} [props.isCaptain=false] - 캡틴 여부를 판단
  * @param {() => void} [props.onClick] - 프로필을 클릭
  * @param {() => void} [props.onEdit] - 편집 버튼을 클릭시
  */
@@ -19,6 +22,7 @@ interface ProfileProps {
   size?: 'small' | 'header' | 'large' | 'full';
   imageUrl?: string;
   editable?: boolean;
+  isCaptain?: boolean;
   onClick?: () => void;
   onEdit?: () => void;
 }
@@ -27,24 +31,35 @@ export function Profile({
   size = 'full',
   imageUrl,
   editable = false,
+  isCaptain = false,
   onClick,
   onEdit,
 }: ProfileProps) {
-  const finalSize = editable ? 'large' : size;
+  const finalSize = editable || isCaptain ? 'large' : size;
 
   // 추후 디자인에 따라 수정
   const sizeClasses = {
     small: 'w-6 h-6',
-    header: 'sm:w-7 sm:h-7 md:w-[40px] md:h-[40px] lg:w-[40px] lg:w-[40px]',
+    header: 'sm:w-7 sm:h-7 md:w-[40px] md:h-[40px] lg:w-[40px] lg:h-[40px]',
     large: 'w-14 h-14',
     full: 'w-full h-full',
+    captain: 'w-20 h-20',
   };
 
   return (
-    <div className={`relative inline-block ${sizeClasses[finalSize]}`}>
+    <div
+      className={cn(
+        'relative inline-block',
+        isCaptain ? sizeClasses.captain : sizeClasses[finalSize],
+      )}
+    >
       <button
         type="button"
-        className={`relative ${sizeClasses[finalSize]} overflow-hidden rounded-full`}
+        className={cn(
+          'relative overflow-hidden rounded-full',
+          isCaptain ? sizeClasses.captain : sizeClasses[finalSize],
+          !editable && 'pointer-events-none',
+        )}
         onClick={onClick}
       >
         <div className="relative h-full w-full rounded-full">
@@ -57,18 +72,22 @@ export function Profile({
           />
         </div>
       </button>
-      {editable && (
+      {editable && !isCaptain && (
         <button
           type="button"
           className="absolute -right-1 bottom-2 z-20 h-5 w-5 rounded-full"
           onClick={(e) => {
-            // 추후 수정예정
             e.stopPropagation();
             if (onEdit) onEdit();
           }}
         >
           <Image src={editImage} alt="수정 버튼" width={24} height={24} />
         </button>
+      )}
+      {isCaptain && (
+        <div className="absolute bottom-0 right-0 z-20">
+          <Image src={captainCheck} alt="캡틴 배지" width={24} height={24} />
+        </div>
       )}
     </div>
   );
