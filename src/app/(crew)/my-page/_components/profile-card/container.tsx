8b870103dfb04ac9b570658fe54b95cc01059da8
HaeJungg <gpwnd0120@gmail.com>
2024-11-18 13:15:01 +0900

    ✨ feat: 프로필 이미지 수정 api 추가, 드롭다운 추가

--- src/app/(crew)/my-page/_components/profile-card/container.tsx
+++ src/app/(crew)/my-page/_components/profile-card/container.tsx
@@ -1,35 +1,75 @@
 'use client';
 
 import { useEffect, useState } from 'react';
+import { toast } from 'react-toastify';
 import { useRouter } from 'next/navigation';
+import { fetchUpdatedUser, updateUserProfile } from '@/src/_apis/auth/user-apis';
 import { useAuthStore } from '@/src/store/use-auth-store';
+import { User } from '@/src/types/auth';
 import ProfileCardPresenter from './presenter';
 
 export default function ProfileCard() {
   const router = useRouter();
-  const { user, isAuth } = useAuthStore();
+  const { isAuth, setUser } = useAuthStore();
+  const [user, setLocalUser] = useState<User | null>(null);
   const [isLoading, setIsLoading] = useState(true);
+  const [profileImageUrl, setProfileImageUrl] = useState<string>('');
 
   useEffect(() => {
-    // 로딩 상태를 위해 약간의 지연 추가
-    const timer = setTimeout(() => {
+    const timer = setTimeout(async () => {
       if (!isAuth) {
         router.push('/login');
       } else {
-        setIsLoading(false);
+        try {
+          const updatedUser = await fetchUpdatedUser();
+          setLocalUser(updatedUser);
+          setUser(updatedUser);
+          setProfileImageUrl(updatedUser.profileImageUrl);
+          setIsLoading(false);
+        } catch (error) {
+          toast.error('유저 정보를 가져오는 데 실패했습니다.');
+        }
       }
     }, 500);
 
     return () => clearTimeout(timer);
-  }, [isAuth, router]);
+  }, [isAuth, router, setUser]);
 
   if (isLoading) return <div>로딩 중...</div>;
 
   if (!user) return <div>유저 정보를 불러오지 못했습니다.</div>;
 
+  // 파일 선택 핸들러
   const handleEdit = () => {
-    // 프로필 수정 기능 추가
+    const input = document.createElement('input');
+    input.type = 'file';
+    input.accept = '.png,.jpg,.jpeg';
+    input.onchange = async (event) => {
+      const file = (event.target as HTMLInputElement)?.files?.[0];
+      if (file) {
+        if (file.size > 5 * 1024 * 1024) {
+          alert('5MB 이하의 파일만 업로드 가능합니다.');
+          return;
+        }
+
+        try {
+          await updateUserProfile(file);
+
+          const tempUrl = URL.createObjectURL(file);
+          setProfileImageUrl(tempUrl);
+
+          const updatedUser = await fetchUpdatedUser();
+
+          const newProfileImageUrl = `${updatedUser.profileImageUrl}?timestamp=${new Date().getTime()}`;
+          setProfileImageUrl(newProfileImageUrl);
+          setUser({ ...updatedUser, profileImageUrl: newProfileImageUrl });
+        } catch (error) {
+          toast.error('파일 업로드에 실패했습니다.');
+        }
+      }
+    };
+    input.click();
   };
 
-  return <ProfileCardPresenter data={user} onEdit={handleEdit} />;
+  return <ProfileCardPresenter data={{ ...user, profileImageUrl }} onEdit={handleEdit} />;
 }
