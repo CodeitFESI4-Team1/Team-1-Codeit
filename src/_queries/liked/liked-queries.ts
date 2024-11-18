b5fd82910bee11ea1f6b47778885dc2e36a66cbc
HaeJungg <gpwnd0120@gmail.com>
2024-11-18 17:43:35 +0900

    🐛 fix: 페이지 0부터 시작하도록 수정

--- src/_queries/liked/liked-queries.ts
+++ src/_queries/liked/liked-queries.ts
@@ -4,6 +4,6 @@ import { getLikedList } from '@/src/_apis/liked/liked-apis';
 export function useGetLikedListQuery(page: number) {
   return useQuery({
     queryKey: ['likedList', page],
-    queryFn: () => getLikedList(page),
+    queryFn: () => getLikedList(page - 1), // 1 -> 0 기반으로 변환
   });
 }
