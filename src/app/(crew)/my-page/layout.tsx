76f5456686f1805a46c0b5fb90c1d39c6fd0591e
조예은 <yeeunee73@gmail.com>
2024-11-19 17:52:56 +0900

    ♻️ Refactor: my-review의 탭 동작 수정

---
+++ src/app/(crew)/my-page/layout.tsx
@@ -0,0 +1,14 @@
+import ProfileCardContainer from '@/src/app/(crew)/my-page/_components/profile-card/container';
+import ReviewTabs from './_components/review-tabs';
+
+export default function MyPage() {
+  return (
+    <div className="container mx-auto my-0 min-h-screen max-w-pc bg-gray-50 px-3 py-11 md:px-8 lg:px-11">
+      <div className="lg:gap-4.5 flex flex-col gap-3 md:gap-4">
+        <ProfileCardContainer />
+      </div>
+      <ReviewTabs />
+      <div />
+    </div>
+  );
+}
