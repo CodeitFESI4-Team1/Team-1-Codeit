e8b71f2224f421b0f8241fa2cc4781d6cf440be0
leeyoul <2yulrang@gmail.com>
2024-11-19 11:23:26 +0900

    Feat/132/create edit gathering api 연결 (#133)
    * 🚚 Chore: 파일 이동동
    
    * ✨ Feat: 약속 만들기 기능 추가
    
    * 🐛 Fix: 달력만 선택해도 폼 통과되는 문제 해결
    
    * 🐛 Fix: 이미지 업로드 시점 변경

--- src/app/(crew)/crew/create/page.tsx
+++ src/app/(crew)/crew/create/page.tsx
@@ -2,7 +2,6 @@
 
 import Image from 'next/image';
 import { Loader } from '@mantine/core';
-import { getImageUrl } from '@/src/_apis/image/get-image-url';
 import { useCreateCrewQuery } from '@/src/_queries/crew/crew-detail-queries';
 import CreateCrewForm from '@/src/app/(crew)/crew/create/_components/create-crew-form';
 import { CreateCrewFormTypes, CreateCrewRequestTypes } from '@/src/types/create-crew';
--- src/app/(crew)/crew/detail/[id]/_components/create-gathering/index.tsx
+++ src/app/(crew)/crew/detail/[id]/_components/create-gathering/index.tsx
@@ -7,7 +7,7 @@ import CreateGatheringModalContainer from '@/src/app/(crew)/crew/detail/[id]/_co
 import Button from '@/src/components/common/input/button';
 import { CreateGatheringFormTypes } from '@/src/types/gathering-data';
 
-export default function CreateGathering() {
+export default function CreateGathering({ crewId }: { crewId: number }) {
   const { isAuth } = useAuthStore();
   const router = useRouter();
   const [opened, { open, close }] = useDisclosure(false);
@@ -34,7 +34,12 @@ export default function CreateGathering() {
       <Button type="button" className="btn-filled px-4" onClick={handleButtonClick}>
         약속 만들기
       </Button>
-      <CreateGatheringModalContainer opened={opened} close={close} data={initialValue} />
+      <CreateGatheringModalContainer
+        crewId={crewId}
+        opened={opened}
+        close={close}
+        data={initialValue}
+      />
     </>
   );
 }
