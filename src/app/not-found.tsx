f48404415ecbd2ac86c73e33876dd6f053115de7
Lee Youl <2yulrang@gmail.com>
2024-11-20 09:51:05 +0900

    💄 Design: 디자인 개선

--- src/app/not-found.tsx
+++ src/app/not-found.tsx
@@ -1,16 +1,22 @@
+'use client';
+
 import Link from 'next/link';
+import { useMediaQuery } from '@mantine/hooks';
+import { theme } from '../styles/theme';
 
 export default function NotFound() {
+  const isMobile = useMediaQuery(`(max-width: ${theme.breakpoints.md})`);
+
   return (
-    <div className="flex h-screen items-center justify-center">
-      <div className="relative">
+    <div className="h-screen md:flex md:items-center md:justify-center">
+      <div className="flex h-full flex-col items-center justify-center gap-10 md:relative md:h-auto">
         <svg
           xmlns="http://www.w3.org/2000/svg"
           xmlnsXlink="http://www.w3.org/1999/xlink"
-          width="567"
+          width="500"
           height="300"
           className="w-full"
-          viewBox="0 0 567 300"
+          viewBox="0 0 500 300"
         >
           <defs>
             <path
@@ -28,35 +34,35 @@ export default function NotFound() {
               <use className="animate-wave" href="#wave" />
               <rect x="0" y="180" width="100%" height="150" />
             </clipPath>
-            <linearGradient id="grad-front" x1="-22%" y1="0" x2="122%" y2="100%">
-              <stop offset="0%" stop-color="#60A5FA" stop-opacity="1" />
-              <stop offset="100%" stop-color="#3388FF" stop-opacity="1" />
+            <linearGradient id="grad-front" x1="0%" y1="0" x2="100%" y2="100%">
+              <stop offset="0%" stopColor="#60A5FA" stopOpacity="1" />
+              <stop offset="100%" stopColor="#3388FF" stopOpacity="1" />
             </linearGradient>
             <text
               id="clipped-wave"
-              x="0"
+              x={isMobile ? '50' : '0'}
               y="260"
-              font-family="Helvetica"
-              font-weight="bold"
-              font-size="18.75rem"
+              fontFamily="Helvetica"
+              fontWeight="bold"
+              fontSize={isMobile ? '15rem' : '18.75rem'}
             >
               404
             </text>
           </defs>
           <use
             href="#clipped-wave"
-            clip-path="url(#wave-clip-middle)"
+            clipPath="url(#wave-clip-middle)"
             fill="url(#grad-front)"
-            fill-opacity="0.88"
+            fillOpacity="0.88"
           />
           <use
             href="#clipped-wave"
-            clip-path="url(#wave-clip-front)"
+            clipPath="url(#wave-clip-front)"
             fill="url(#grad-front)"
-            fill-opacity="0.77"
+            fillOpacity="0.77"
           />
         </svg>
-        <div className="absolute -bottom-10 flex w-full translate-y-full flex-col items-center gap-9">
+        <div className="flex w-full flex-col items-center gap-9 pb-10 md:absolute md:-bottom-10 md:translate-y-full">
           <p className="text-2xl font-semibold text-gray-900">
             &quot;페이지를 찾을 수 없습니다. 😢&quot;
           </p>
