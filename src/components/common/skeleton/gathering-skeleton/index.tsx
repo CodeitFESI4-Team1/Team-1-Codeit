9e230823a59d6d6ece2777e5e421bf6d14d9618f
HaeJungg <gpwnd0120@gmail.com>
2024-11-20 17:57:26 +0900

    💄 design: 스켈레톤 ui수정

--- src/components/common/skeleton/gathering-skeleton/index.tsx
+++ src/components/common/skeleton/gathering-skeleton/index.tsx
@@ -2,14 +2,20 @@ import { Skeleton } from '@mantine/core';
 
 export default function GatheringSkeleton() {
   return (
-    <div className="flex flex-col overflow-hidden rounded-xl">
-      <Skeleton className="h-[160px] w-full" />
-      <div className="relative flex min-h-[184px] flex-col gap-2 p-4">
-        <Skeleton className="h-4 w-40" />
-        <Skeleton className="h-4 w-32" />
-        <Skeleton className="h-4 w-24" />
-        <Skeleton circle className="absolute right-4 top-4 h-8 w-8" />
-        <Skeleton className="absolute bottom-4 left-4 right-4 h-10 w-auto" />
+    <div className="relative h-[380px] w-full overflow-hidden rounded-lg bg-white shadow-sm">
+      <div className="relative h-40 w-full">
+        <Skeleton className="h-full w-full rounded-t-lg" />
+      </div>
+      <div className="flex min-h-[220px] flex-col justify-between p-4">
+        <div>
+          <Skeleton className="mb-2 h-6 w-1/3" />
+          <Skeleton className="mb-4 h-6 w-2/3" />
+          <Skeleton className="h-4 w-1/2" />
+        </div>
+        <div className="mt-6">
+          <Skeleton className="h-4 w-1/3" />
+          <div className="mt-4 h-10" />
+        </div>
       </div>
     </div>
   );
