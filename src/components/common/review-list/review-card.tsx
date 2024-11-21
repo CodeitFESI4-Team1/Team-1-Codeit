4d15e0197996a1af55805a0bd815ce471365c30f
HaeJungg <gpwnd0120@gmail.com>
2024-11-21 15:44:19 +0900

    🐛 fix: 사용하지 않는 import 삭제

--- src/components/common/review-list/review-card.tsx
+++ src/components/common/review-list/review-card.tsx
@@ -1,6 +1,6 @@
 'use client';
 
-import React, { useState } from 'react';
+import React from 'react';
 import { toast } from 'react-toastify';
 import { useRouter } from 'next/navigation';
 import { useDisclosure } from '@mantine/hooks';
