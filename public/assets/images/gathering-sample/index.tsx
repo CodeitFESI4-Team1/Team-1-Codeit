3ce533144b68b1f25203825cb9d420bad050c9c3
Lee Youl <2yulrang@gmail.com>
2024-11-21 09:26:10 +0900

    🍱 Fix: 샘플 이미지 교체

--- public/assets/images/gathering-sample/index.tsx
+++ public/assets/images/gathering-sample/index.tsx
@@ -4,9 +4,9 @@ import ImgGatheringSample03 from './gathering-sample-3.jpg';
 
 const ImgGatheringSamples = [ImgGatheringSample01, ImgGatheringSample02, ImgGatheringSample03];
 const ImgGatheringSampleUrls = [
-  'https://crewcrew.s3.ap-northeast-2.amazonaws.com/crew/1ff61110-e238-4fd6-8736-e6f03483f4df',
-  'https://crewcrew.s3.ap-northeast-2.amazonaws.com/crew/85ad6a48-a1cb-4c8f-b713-94b9e18b023a',
-  'https://crewcrew.s3.ap-northeast-2.amazonaws.com/crew/60bc093c-cb3a-4628-93b9-fbf19e47a1ac',
+  'https://crewcrew.s3.ap-northeast-2.amazonaws.com/crew/e276276c-c79e-4654-a2b2-279bccb7ca05',
+  'https://crewcrew.s3.ap-northeast-2.amazonaws.com/crew/69d4f72f-8bad-4e9e-894c-9e8c9b57db2d',
+  'https://crewcrew.s3.ap-northeast-2.amazonaws.com/crew/d3c507f4-0391-4acb-8bc3-ae5379bdb058',
 ];
 
 export default ImgGatheringSampleUrls;
